f3fd10ce040944910a7ced4e832d58d1a26d19e5
Mario <mario@ipor.io>
2021-12-01 08:54:30 +0100

    IL-329, Exponential Moving Average visible in dev tool

--- app/src/IporIndexList.js
+++ app/src/IporIndexList.js
@@ -1,36 +1,50 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import {toDate} from "./utils.js";
+import { toDate } from "./utils.js";
 
-export default (indexes) => (
+export default (indexes) =>
     indexes && indexes.length > 0 ? (
         <table className="table">
             <thead>
-            <tr>
-                <th scope="col">Asset</th>
-                <th scope="col">IPOR Value</th>
-                <th scope="col">Interest Bearing Token Price</th>
-                <th scope="col">Block Timestamp</th>
-            </tr>
+                <tr>
+                    <th scope="col">Asset</th>
+                    <th scope="col">IPOR Value</th>
+                    <th scope="col">Interest Bearing Token Price</th>
+                    <th scope="col">Exponential Moving Average</th>
+                    <th scope="col">Block Timestamp</th>
+                </tr>
             </thead>
             <tbody>
-            {indexes.map(index => (
-                <tr key={index.asset}>
-                    <td>{index.asset}</td>
-                    <td>
-                        {index.asset === "DAI" ? index.indexValue / 1000000000000000000 : index.indexValue / 1000000}
-                        <br/>
-                        <small>{index.indexValue}</small>
-                    </td>
-                    <td>
-                        {index.asset === "DAI" ? index.ibtPrice  / 1000000000000000000 : index.ibtPrice  / 1000000}
-                        <br/>
-                        <small>{index.ibtPrice}</small>
-                    </td>
-                    <td>{toDate(index.blockTimestamp)}</td>
-                </tr>
-            ))}
+                {indexes.map((index) => (
+                    <tr key={index.asset}>
+                        <td>{index.asset}</td>
+                        <td>
+                            {index.asset === "DAI"
+                                ? index.indexValue / 1000000000000000000
+                                : index.indexValue / 1000000}
+                            <br />
+                            <small>{index.indexValue}</small>
+                        </td>
+                        <td>
+                            {index.asset === "DAI"
+                                ? index.ibtPrice / 1000000000000000000
+                                : index.ibtPrice / 1000000}
+                            <br />
+                            <small>{index.ibtPrice}</small>
+                        </td>
+                        <td>
+                            {index.asset === "DAI"
+                                ? index.exponentialMovingAverage /
+                                  1000000000000000000
+                                : index.exponentialMovingAverage / 1000000}
+                            <br />
+                            <small>{index.exponentialMovingAverage}</small>
+                        </td>
+                        <td>{toDate(index.blockTimestamp)}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
-    ) : <p>No IPOR Indexes yet</p>
-);
+    ) : (
+        <p>No IPOR Indexes yet</p>
+    );
