0cf46abedbb3640ad8149bfe560eb8f076b0a15d
Mario <mario@ipor.io>
2021-12-15 16:41:05 +0100

    IL-206 .

--- test-new/SoapIndicatorLogic.test.js
+++ test-new/SoapIndicatorLogic.test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const ONE_18DEC = BigInt("1000000000000000000");
+const ONE_16DEC = BigInt("10000000000000000");
 
 describe("SoapIndicatorLogic", () => {
     let SoapIndicatorLogic;
@@ -19,4 +20,26 @@ describe("SoapIndicatorLogic", () => {
         mockSoapIndicatorLogic = await MockSoapIndicatorLogic.deploy();
         await mockSoapIndicatorLogic.deployed();
     });
+
+	// it("should calculate interest rate when open position - simple case - 18 decimals", async () => {
+	// 	//given
+    //     const soapIndicator = prepareSoapIndicatorPfCaseD18();
+    //     const  derivativeNotional = BigInt(10000) * ONE_18DEC;
+    //     const derivativeFixedInterestRate = BigInt(4) * ONE_16DEC;
+
+    //     //when
+    //     const actualInterestRate = soapIndicator
+    //         .calculateInterestRateWhenOpenPosition(
+    //             derivativeNotional,
+    //             derivativeFixedInterestRate
+    //         );
+
+    //     //then
+    //     const expectedInterestRate = BigInt(66666666666666667);
+    //     Assert.equal(
+    //         expectedInterestRate,
+    //         actualInterestRate,
+    //         "Wrong interest rate when open position"
+    //     );
+	// });
 });
