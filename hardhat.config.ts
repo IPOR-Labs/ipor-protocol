4e559cec914d7ea153aa9b60f77be2e2f60cc592
Mario <mario@ipor.io>
2022-02-01 12:10:26 +0100

    IL-538

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-tracer");
-require("solidity-coverage");
-require("@nomiclabs/hardhat-web3");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-tracer";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-web3";
+import "dotenv";
+// require("@nomiclabs/hardhat-waffle");
+// require("hardhat-tracer");
+// require("solidity-coverage");
+// require("@nomiclabs/hardhat-web3");
 require("dotenv").config();
 
 if (process.env.REPORT_GAS === "true") {
@@ -24,7 +30,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+export default {
     solidity: {
         version: "0.8.9",
         settings: {
