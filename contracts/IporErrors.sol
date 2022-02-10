75eae86a4faf930c94aa1b73a00b39d8b5c988cb
Mario <mario@ipor.io>
2022-02-10 15:20:49 +0100

    IL-570 remove commented code

--- contracts/IporErrors.sol
+++ contracts/IporErrors.sol
@@ -50,9 +50,6 @@ library IporErrors {
         public constant MILTON_CANNOT_CLOSE_DERIVATE_SENDER_IS_NOT_BUYER_AND_NO_DERIVATIVE_MATURITY =
         "IPOR_16";
 
-    //@notice Swap should be in ACTIVE state
-    // string public constant MILTON_DERIVATIVE_IS_INACTIVE = "IPOR_17";
-
     //@notice Input arrays which should have the same length - mismatch
     string public constant WARREN_INPUT_ARRAYS_LENGTH_MISMATCH = "IPOR_18";
 
