9840161cd6641b5a18e9bd00e790c85f5d763c78
Mario <mario@ipor.io>
2022-02-16 17:15:35 +0100

    IL-598 max spread is set to 3% instead of 0.3%

---
+++ migrations/5_upgrade_contracts_001.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: "../.env" });
+const keccak256 = require("keccak256");
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+const MiltonSpreadModel = artifacts.require("MiltonSpreadModel");
+module.exports = async function (deployer, _network, addresses) {
+    console.log("Upgrade Smart Contracts...");
+
+    await upgradeContract(MiltonSpreadModel);
+
+    console.log("Congratulations! Upgrade Smart Contracts...");
+};
+
+async function upgradeContract(Contract) {
+    const proxy = await Contract.deployed();
+    const instance = await upgradeProxy(proxy.address, Contract);
+    console.log("Upgraded! ", instance.address);
+}
