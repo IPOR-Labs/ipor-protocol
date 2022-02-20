98d8f86bff172fd587ad36d4ac2ce5cc7dcb9331
Mario <mario@ipor.io>
2022-02-20 19:16:01 +0100

    IL-171

--- contracts/security/IporOwnable.sol
+++ contracts/security/IporOwnable.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {IporErrors} from "../IporErrors.sol";
 
-contract IporOwnableUpgradeable is OwnableUpgradeable {
+contract IporOwnable is Ownable {
     address private _appointedOwner;
 
     event AppointedToTransferOwnership(address indexed appointedOwner);
