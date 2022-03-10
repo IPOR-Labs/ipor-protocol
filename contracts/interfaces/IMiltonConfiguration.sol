312dba3ec2a2ef0f3f96f159ee07e31612fe7589
Piotr Rzonsowski <pete@ipor.io>
2022-03-10 21:21:13 +0100

    IL-495

--- contracts/interfaces/IMiltonConfiguration.sol
+++ contracts/interfaces/IMiltonConfiguration.sol
@@ -4,37 +4,27 @@ pragma solidity 0.8.9;
 interface IMiltonConfiguration {
     event JosephUpdated(address indexed newJoseph);
 
-    function getMaxSwapCollateralAmount() external pure returns (uint256);    
+    function getMaxSwapCollateralAmount() external pure returns (uint256);
+
+    function getMaxSlippagePercentage() external pure returns (uint256);
 
     function getMaxLpUtilizationPercentage() external pure returns (uint256);
 
-    function getMaxLpUtilizationPerLegPercentage()
-        external
-        pure
-        returns (uint256);
+    function getMaxLpUtilizationPerLegPercentage() external pure returns (uint256);
 
     function getIncomeTaxPercentage() external pure returns (uint256);
 
     function getOpeningFeePercentage() external pure returns (uint256);
 
-    function getOpeningFeeForTreasuryPercentage()
-        external
-        pure
-        returns (uint256);
+    function getOpeningFeeForTreasuryPercentage() external pure returns (uint256);
 
     function getIporPublicationFeeAmount() external pure returns (uint256);
 
     function getLiquidationDepositAmount() external pure returns (uint256);
 
-    function getMaxCollateralizationFactorValue()
-        external
-        pure
-        returns (uint256);
+    function getMaxCollateralizationFactorValue() external pure returns (uint256);
 
-    function getMinCollateralizationFactorValue()
-        external
-        pure
-        returns (uint256);
+    function getMinCollateralizationFactorValue() external pure returns (uint256);
 
     function getMiltonSpreadModel() external view returns (address);
 
