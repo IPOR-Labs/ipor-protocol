4d1484968b2309fb877f6ed3654b1501b529a5af
Piotr Rzonsowski <pete@ipor.io>
2022-03-29 22:25:40 +0200

    IL-701 Milton Maintenance

--- test/utils/Constants.ts
+++ test/utils/Constants.ts
@@ -30,6 +30,7 @@ export const USD_15_000_18DEC = BigNumber.from("15000").mul(N1__0_18DEC);
 export const USD_19_997_18DEC = BigNumber.from("19997").mul(N1__0_18DEC);
 export const USD_20_000_18DEC = BigNumber.from("20000").mul(N1__0_18DEC);
 export const USD_28_000_18DEC = BigNumber.from("28000").mul(N1__0_18DEC);
+export const USD_50_000_18DEC = BigNumber.from("50000").mul(N1__0_18DEC);
 export const USD_10_000_000_18DEC = BigNumber.from("10000000").mul(N1__0_18DEC);
 
 export const PERCENTAGE_2_5_18DEC = BigNumber.from("25").mul(N0__001_18DEC);
