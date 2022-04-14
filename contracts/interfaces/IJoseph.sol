4f8ecee221e29d1d8da01676e398a94c2c1654cf
Mario <mario@ipor.io>
2022-04-14 11:04:41 +0200

    Code review

--- contracts/interfaces/IJoseph.sol
+++ contracts/interfaces/IJoseph.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.9;
 
 /// @title Interface for interaction with Joseph - smart contract responsible
