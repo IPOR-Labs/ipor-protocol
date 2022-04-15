1ccd6af39f3f9d92157c9ef21e3f4b838583aa75
Wookash <wookash@ipor.io>
2022-04-15 16:33:57 +0200

    improved documentation

--- contracts/amm/libraries/types/AmmMiltonTypes.sol
+++ contracts/amm/libraries/types/AmmMiltonTypes.sol
@@ -3,15 +3,30 @@ pragma solidity 0.8.9;
 
 import "../../../interfaces/types/IporTypes.sol";
 
+
+/// @notice The types used in Milton's interface. 
+/// @dev All values, where applicable, are represented in 18 decimals. 
 library AmmMiltonTypes {
     struct BeforeOpenSwapStruct {
+        /// @notice Sum of all asset transfered when opening swap. It includes the collateral, fees and desposits. 
+        /// @dev The amount is represented in 18 decimals regardless of the decimals of the asset.
         uint256 wadTotalAmount;
+        /// @notice Swap's collateral. 
         uint256 collateral;
+        /// @notice Swap's notional amount. 
         uint256 notional;
+        /// @notice The part of the opening fee that will be added to the liquidity pool balance. 
         uint256 openingFeeLPAmount;
+        /// @notice Part of the opening fee that will be added to the treasury balance.
         uint256 openingFeeTreasuryAmount;
+        /// @notice Amount of asset set aside for the oracle subsidization.
         uint256 iporPublicationFeeAmount;
+        /// @notice Refundable deposit blocked for the entity that will close the swap. 
+        /// For more information on how the liquidations work refer to the documentation. 
+        /// https://ipor-labs.gitbook.io/ipor-labs/automated-market-maker/liquidations
         uint256 liquidationDepositAmount;
+        /// @notice The struct describing the IPOR and its params calculated for the time when it was most recently updated and the change that took place since the update. 
+        /// Namely, the interest that would be computed into IBT should the rebalance occur.
         IporTypes.AccruedIpor accruedIpor;
     }
 }
