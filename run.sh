f726b63f3bcfd82b145d2f5d4cd32885a1cb517d
Mario <mario@ipor.io>
2022-04-27 12:00:28 +0200

    IL-871 cr fix

--- run.sh
+++ run.sh
@@ -381,7 +381,7 @@ if [ $IS_MIGRATE_SC = "YES" ]; then
 
   cd "${DIR}"
   npm run compile:truffle 2>&1| tee ".logs/${ENV_PROFILE}/compile/${LAST_MIGRATION_DATE}_compile.log"
-  yarn run hardhat export-abi
+  npm run export-abi
   export ETH_BC_NETWORK_NAME
   npm run migrate:truffle 2>&1| tee ".logs/${ENV_PROFILE}/migrate/${LAST_MIGRATION_DATE}_migrate.log"
 fi
@@ -396,7 +396,7 @@ if [ $IS_MIGRATE_WITH_CLEAN_SC = "YES" ]; then
 
   cd "${DIR}"
   npm run compile:truffle 2>&1| tee ".logs/${ENV_PROFILE}/compile/${LAST_MIGRATION_DATE}_compile.log"
-  yarn run hardhat export-abi
+  npm run export-abi
   export ETH_BC_NETWORK_NAME
   npm run migrate:truffle-reset 2>&1| tee ".logs/${ENV_PROFILE}/migrate/${LAST_MIGRATION_DATE}_migrate.log"
 fi
