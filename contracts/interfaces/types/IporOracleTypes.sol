d47f862e16b021d33f30544de78c5ed8cdddac8b
Mario <mario@ipor.io>
2022-05-10 15:56:04 +0200

    IL-938, change IporOracleTypes.IPOR struct field size: - indexValue - exponentialMovingAverage - exponentialWeightedMovingVariance

--- contracts/interfaces/types/IporOracleTypes.sol
+++ contracts/interfaces/types/IporOracleTypes.sol
@@ -5,16 +5,16 @@ pragma solidity 0.8.9;
 library IporOracleTypes {
     //@notice IPOR Index Structure for a given asset
     struct IPOR {
-        //@notice Tiestamp of most recent IPOR index update, action performed by Charlie (refer to the documentation for more details)
-        uint32 lastUpdateTimestamp;
-        //@notice IPOR Index value.
-        uint128 indexValue;
         //@notice Quasi Interest Bearing Token Price - IBT Price without division by year in seconds
         uint128 quasiIbtPrice;
         /// @notice Exponential Moving Average
         /// @dev used in calculating spread in MiltonSpreadModel smart contract
-        uint128 exponentialMovingAverage;
+        uint64 exponentialMovingAverage;
         //@notice exponential weighted moving variance - required for calculating spread in Milton
-        uint128 exponentialWeightedMovingVariance;
+        uint64 exponentialWeightedMovingVariance;		
+		//@notice IPOR Index value.
+        uint64 indexValue;
+		//@notice Tiestamp of most recent IPOR index update, action performed by Charlie (refer to the documentation for more details)
+        uint32 lastUpdateTimestamp;                
     }
 }
