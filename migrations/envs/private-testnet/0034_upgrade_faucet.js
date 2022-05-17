c248123bcad2ccaa6261af915bfe7f5c091ade22
Mario <mario@ipor.io>
2022-05-17 15:47:11 +0200

    Redundant TestnetFaucetV2

--- migrations/envs/private-testnet/0034_upgrade_faucet.js
+++
@@ -1,7 +0,0 @@
-const func = require("../../libs/json_func.js");
-const script = require("../../libs/contracts/setup/faucet/0002_upgrade_faucet.js");
-
-module.exports = async function (deployer, _network, addresses) {
-    await script(deployer, _network, addresses);
-    await func.updateLastCompletedMigration();
-};
