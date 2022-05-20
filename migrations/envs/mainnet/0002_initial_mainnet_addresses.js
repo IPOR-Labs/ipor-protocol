93b0409c7569e31d45a176b277032e72b6d0258e
Mario <mario@ipor.io>
2022-05-20 14:37:04 +0200

    Migration.

--- migrations/envs/mainnet/0002_initial_mainnet_addresses.js
+++ migrations/envs/mainnet/0002_initial_mainnet_addresses.js
@@ -1,5 +1,5 @@
-const keys = require("../../json_keys.js");
-const func = require("../../json_func.js");
+const func = require("../../libs/json_keys.js");
+const func = require("../../libs/json_func.js");
 
 module.exports = async function (deployer, _network, addresses) {
     const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
