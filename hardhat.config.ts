2d8807dd3bb4194368c0fa1b01638ade6eae58b3
Mario <mario@ipor.io>
2022-05-26 14:00:47 +0200

    IL-993

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,16 +27,6 @@ if (process.env.FORK_ENABLED === "true") {
     jobs = 1;
 }
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
