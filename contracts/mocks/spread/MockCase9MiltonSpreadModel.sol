dd71d9fdff9ea7718caf9647bf23ac042287e23c
Mario <mario@ipor.io>
2022-06-04 21:31:10 +0200

    IL-993 revert mocks

---
+++ contracts/mocks/spread/MockCase9MiltonSpreadModel.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+import "./MockBaseMiltonSpreadModel.sol";
+
+contract MockCase9MiltonSpreadModel is MockBaseMiltonSpreadModel {
+    function _getSpreadPremiumsMaxValue() internal pure virtual override returns (uint256) {
+        return 300000000000000000;
+    }
+
+    function _getDCKfValue() internal pure virtual override returns (uint256) {
+        return 1000000000000000;
+    }
+
+    function _getDCLambdaValue() internal pure virtual override returns (uint256) {
+        return 0;
+    }
+
+    function _getDCKOmegaValue() internal pure virtual override returns (uint256) {
+        return 30000000000000000;
+    }
+
+    function _getDCMaxLiquidityRedemptionValue() internal pure virtual override returns (uint256) {
+        return 1000000000000000000;
+    }
+
+}
