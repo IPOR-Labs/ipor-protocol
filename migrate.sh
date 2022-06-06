f40f91b71522039830f5c065f0a11c9aa04c5b7b
adam <adam@ipor.io>
2022-06-06 12:32:53 +0200

    IL-925 simple migration script - finishing

--- migrate.sh
+++ migrate.sh
@@ -2,8 +2,6 @@
 
 set -e -o pipefail
 
-POSITIONAL_ARGS=()
-
 BEGIN_FROM_MIGRATION=0
 MIGRATION_PREFIX="migration-"
 DRY_RUN=false
@@ -32,7 +30,6 @@ while [[ $# -gt 0 ]]; do
   *)
     echo "Unexpected option: $1"
     exit 1
-
     ;;
   esac
 done
@@ -42,20 +39,21 @@ CURRENT_MIGRATION_NUMBER=${BEGIN_FROM_MIGRATION}
 while true; do
   MIGRATION_TAG_NAME="${MIGRATION_PREFIX}${CURRENT_MIGRATION_NUMBER}"
 
-  echo CURRENT_MIGRATION_NUMBER="${CURRENT_MIGRATION_NUMBER}"
-  echo MIGRATION_TAG_NAME="${MIGRATION_TAG_NAME}"
-
   echo -e "\n\e[32mCheckout git tag ${MIGRATION_TAG_NAME}\e[0m\n"
-  if [ $DRY_RUN != true ]; then
+  if [[ $DRY_RUN != true ]]; then
     git checkout "${MIGRATION_TAG_NAME}"
   fi
 
   echo -e "\n\e[32mStart migration\e[0m\n"
-  if [ $DRY_RUN != true ]; then
+  if [[ $DRY_RUN != true ]]; then
     ./run.sh migrate
   fi
 
-  CURRENT_MIGRATION_NUMBER=$((CURRENT_MIGRATION_NUMBER + 1))
-  echo -e "\n\e[32mNext tag name: ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
-
+  if [[ "${MIGRATE_TO}" == "${CURRENT_MIGRATION_NUMBER}" ]]; then
+    echo -e "\n\e[32mFinishing migration at ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
+    exit 0
+  else
+    CURRENT_MIGRATION_NUMBER=$((CURRENT_MIGRATION_NUMBER + 1))
+    echo -e "\n\e[32mNext tag name: ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
+  fi
 done
