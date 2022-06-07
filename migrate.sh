cf8c7cf5d433aeebe73f2003a0eb99cc4eb53a42
adam <adam@ipor.io>
2022-06-07 19:48:28 +0200

    IL-925 simple migration script - even better

--- migrate.sh
+++ migrate.sh
@@ -2,38 +2,56 @@
 
 set -e -o pipefail
 
-BEGIN_FROM_MIGRATION=0
-MIGRATION_PREFIX="migration-"
-DRY_RUN=false
-MIGRATION_COMMAND_PARAMS=""
-
 function show_how_to_use() {
   echo -e "usage: \e[32m./migrate.sh\e[0m [cmd1] [cmd2] [cmd3]"
   echo -e ""
   echo -e "commands can by joined together, order of commands doesn't matter, allowed commands:"
-  echo -e "   \e[36m-b | --begin-from-migration\e[0m       from which migration should start"
-  echo -e "   \e[36m-m | --migrate-to\e[0m                 which migration is the end"
-  echo -e "   \e[36m-t | --tag-prefix\e[0m                 tag prefix for migration"
-  echo -e "   \e[36m-d | --dry-run\e[0m                    run without real migration"
-  echo -e "   \e[36m-c | --migration-command-params\e[0m   add extra params to migrate command"
-  echo -e "   \e[34mwithout any parameters\e[0m            run with defaults"
+  echo -e "   \e[36m-s | --start-from-migration\e[0m       from which migration should start"
+  echo -e "   \e[36m-e | --migrate-to\e[0m                 which migration is the end"
+  echo -e "   \e[36m-p | --tag-prefix\e[0m                 TAG prefix for migration"
+  echo -e "   \e[36m-d | --dry-run\e[0m                    run without real migration. Testing is easier in this mode"
+  echo -e "   \e[36m-c | --migration-command-params\e[0m   add extra params to migrate command e.g.:"
+  echo -e "                                     migrationlogs which commits to ipor-migration-state "
+  echo -e "   \e[34m     without any parameters\e[0m       run with defaults"
   echo -e ""
   exit 0
 }
 
+START_FROM_MIGRATION=0
+MIGRATION_PREFIX="migration-"
+DRY_RUN=false
+MIGRATION_COMMAND_PARAMS=""
+MIGRATE_TO=999999999                 # without migrate-to arg use 'infinity' value as default
+
+ENV_FILE="$(pwd)/.env"
+
+if [ -f "${ENV_FILE}" ]; then
+  set -a
+  source "${ENV_FILE}"
+  set +a
+  echo -e "\n\e[32m${ENV_FILE} file was read\e[0m\n"
+fi
+
+LAST_COMPLETED_MIGRATION_FILE_PATH=".ipor/${ENV_PROFILE}-${ETH_BC_NETWORK_NAME}-last-completed-migration.json"
+
+if [ -f "${LAST_COMPLETED_MIGRATION_FILE_PATH}" ]; then
+  LAST_COMPLETED_MIGRATION_NUMBER=$(jq -r ".lastCompletedMigration" "${LAST_COMPLETED_MIGRATION_FILE_PATH}")
+  START_FROM_MIGRATION=$((LAST_COMPLETED_MIGRATION_NUMBER + 1))
+fi
+
 while [[ $# -gt 0 ]]; do
   case $1 in
-  -b | --begin-from-migration)
-    BEGIN_FROM_MIGRATION="$2"
+  -s | --start-from-migration)
+    START_FROM_MIGRATION="$2"
     shift
     shift
     ;;
-  -m | --migrate-to)
+  -e | --migrate-to)
     MIGRATE_TO="$2"
     shift
     shift
     ;;
-  -t | --tag-prefix)
+  -p | --tag-prefix)
     MIGRATION_PREFIX="$2"
     shift
     shift
@@ -59,39 +77,26 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-CURRENT_MIGRATION_NUMBER=${BEGIN_FROM_MIGRATION}
+MIGRATION_REGEX_PATTERN="^${MIGRATION_PREFIX}([0-9])+$"
+TAGS_LIST=$(git tag --list | grep -E "${MIGRATION_REGEX_PATTERN}" | sort -V)
 
-while true; do
-  MIGRATION_TAG_NAME="${MIGRATION_PREFIX}${CURRENT_MIGRATION_NUMBER}"
+for TAG in $TAGS_LIST; do
+  [[ $TAG =~ $MIGRATION_REGEX_PATTERN ]]
+  TAG_NUMBER=${BASH_REMATCH[1]}
 
-  if [ "$(git tag --list "${MIGRATION_TAG_NAME}")" ]; then
-    echo -e "\n\e[32mCheckout git tag ${MIGRATION_TAG_NAME}\e[0m\n"
+  if [[ "${TAG_NUMBER}" -ge "${START_FROM_MIGRATION}" ]] && [[ "${TAG_NUMBER}" -le "${MIGRATE_TO}" ]]; then
+    echo -e "\n\e[32mCheckout git tag ${TAG}\e[0m\n"
     if [[ $DRY_RUN != true ]]; then
-      git checkout "${MIGRATION_TAG_NAME}"
+      git checkout "${TAG}"
     fi
 
-    echo -e "\n\e[32mStart migration\e[0m\n"
+    echo -e "\n\e[32mStart migration from git tag ${TAG}\e[0m\n"
     if [[ $DRY_RUN != true ]]; then
+      # shellcheck disable=SC2086
       ./run.sh migrate ${MIGRATION_COMMAND_PARAMS}
     fi
-
-    if [[ "${MIGRATE_TO}" == "${CURRENT_MIGRATION_NUMBER}" ]]; then
-      echo -e "\n\e[32mFinishing migration at ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
-      exit 0
-    else
-      CURRENT_MIGRATION_NUMBER=$((CURRENT_MIGRATION_NUMBER + 1))
-      echo -e "\n\e[32mNext tag number: ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
-    fi
   else
-    echo -e "\n\e[31mTag ${MIGRATION_TAG_NAME} does not exist. Skipping tag.\e[0m\n"
-  fi
-
-  if [[ "${MIGRATE_TO}" == "${CURRENT_MIGRATION_NUMBER}" ]]; then
-    echo -e "\n\e[32mFinishing migration at ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
-    exit 0
-  else
-    CURRENT_MIGRATION_NUMBER=$((CURRENT_MIGRATION_NUMBER + 1))
-    echo -e "\n\e[32mThe next tag number: ${CURRENT_MIGRATION_NUMBER}\e[0m\n"
+    echo "Skipping tag ${TAG}".
   fi
 
 done
