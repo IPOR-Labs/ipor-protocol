5ca0e81b39593e608edf8deb2818643810903fb2
Mario <mario@ipor.io>
2022-07-07 22:02:39 +0200

    IL-1231 [SC] Upgrade Solidity from 0.8.14 to 0.8.15 (#262)
    # Conflicts:
    #	contracts/libraries/errors/MiltonErrors.sol

--- contracts/amm/pool/Joseph.sol
+++ contracts/amm/pool/Joseph.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
 
 import "../../libraries/errors/IporErrors.sol";
 import "../../libraries/errors/MiltonErrors.sol";
--- contracts/vault/strategies/StrategyAave.sol
+++ contracts/vault/strategies/StrategyAave.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
