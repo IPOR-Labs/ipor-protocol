089f05bad4dac84f0a71850b82c73dc12d6491de
Mario <mario@ipor.io>
2022-07-14 16:10:30 +0200

    IL-1252

--- migrations/envs/mainnet/0028_deploy_joseph_usdt.js
+++ migrations/envs/mainnet/0028_deploy_joseph_usdt.js
@@ -4,6 +4,12 @@ const script = require("../../libs/contracts/deploy/joseph/usdt/0001_initial_dep
 
 module.exports = async function (deployer, _network, addresses) {
     const JosephUsdt = artifacts.require("JosephUsdt");
-    await script(deployer, _network, addresses, JosephUsdt, process.env.SC_MIGRATION_INITIAL_PAUSE_FLAG_JOSEPH);
+    await script(
+        deployer,
+        _network,
+        addresses,
+        JosephUsdt,
+        process.env.SC_MIGRATION_INITIAL_PAUSE_FLAG_JOSEPH
+    );
     await func.updateLastCompletedMigration();
 };
