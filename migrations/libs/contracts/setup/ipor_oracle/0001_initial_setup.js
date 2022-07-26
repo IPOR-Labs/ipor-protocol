70f5879d302245e2a73c892d422fc66d619aec36
Mario <85162486+mario-ipor@users.noreply.github.com>
2022-07-26 09:01:18 +0200

    IL-1277 [SC] Change SC_IPOR_INDEX_UPDATER_PRIV_KEY to SC_MIGRATION_IPOR_INDEX_UPDATER_ADDRESS and remove it from truffle config (#294)

--- migrations/libs/contracts/setup/ipor_oracle/0001_initial_setup.js
+++ migrations/libs/contracts/setup/ipor_oracle/0001_initial_setup.js
@@ -4,7 +4,14 @@ const func = require("../../../json_func.js");
 const IporOracle = artifacts.require("IporOracle");
 
 module.exports = async function (deployer, _network, addresses) {
-    const [admin, iporIndexUpdater, _] = addresses;
+    const [admin, _] = addresses;
+
+    if (!process.env.SC_MIGRATION_IPOR_INDEX_UPDATER_ADDRESS) {
+        throw new Error(
+            "Migration stopped! Environment parameter SC_MIGRATION_IPOR_INDEX_UPDATER_ADDRESS is not set!"
+        );
+    }
+    const iporIndexUpdater = process.env.SC_MIGRATION_IPOR_INDEX_UPDATER_ADDRESS;
 
     const iporOracle = await func.getValue(keys.IporOracleProxy);
 
