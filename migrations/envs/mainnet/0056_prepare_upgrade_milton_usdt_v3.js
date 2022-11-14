c10b8571b14ef4a279b5589e27e6cc4107a7ea76
Mario <mario@ipor.io>
2022-11-14 14:49:55 +0100

    IL-1841

--- migrations/envs/mainnet/0056_prepare_upgrade_milton_usdt_v3.js
+++ migrations/envs/mainnet/0056_prepare_upgrade_milton_usdt_v3.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "../../../.env" });
 const func = require("../../libs/json_func.js");
-const script = require("../../libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3.js");
+const script = require("../../libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3_only_mainnet.js");
 
 module.exports = async function (deployer, _network, addresses) {
     const Milton = artifacts.require("MiltonUsdt");
--- migrations/libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3_only_mainnet.js
+++ migrations/libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3_only_mainnet.js
@@ -1,15 +1,20 @@
 const keys = require("../../../../json_keys.js");
 const func = require("../../../../json_func.js");
 
-const { prepareUpgrade } = require("@openzeppelin/truffle-upgrades");
+const { prepareUpgrade, forceImport } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer, _network, addresses, Milton) {
     const proxyAddress = await func.getValue(keys.MiltonProxyUsdt);
+    const oldImplAddress = await func.getValue(keys.MiltonImplUsdt);
+
+    await forceImport(proxyAddress, oldImplAddress, {
+        deployer: deployer,
+        kind: "uups",
+    });
 
     const implAddress = await prepareUpgrade(proxyAddress, Milton, {
         deployer: deployer,
         kind: "uups",
     });
-
     await func.update(keys.MiltonImplUsdt, implAddress);
 };
