831548a44e66e1c63de7b93f55d0537f871b6552
Mario <mario@ipor.io>
2022-11-14 15:40:31 +0100

    IL-1841

--- migrations/libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3_only_mainnet.js
+++ migrations/libs/contracts/upgrade/milton/usdt/0002_prepare_upgrade_v3_only_mainnet.js
@@ -7,7 +7,7 @@ module.exports = async function (deployer, _network, addresses, Milton) {
     const proxyAddress = await func.getValue(keys.MiltonProxyUsdt);
     const oldImplAddress = await func.getValue(keys.MiltonImplUsdt);
 
-    await forceImport(proxyAddress, oldImplAddress, {
+    await forceImport(oldImplAddress, Milton, {
         deployer: deployer,
         kind: "uups",
     });
