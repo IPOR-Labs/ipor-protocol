fe4002a2307c6d29c5934139ca0174a2cdf69f03
Mario <mario@ipor.io>
2022-11-15 16:07:15 +0100

    IL-1842

--- contracts/amm/MiltonUsdt.sol
+++ contracts/amm/MiltonUsdt.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.16;
 import "./Milton.sol";
 
 contract MiltonUsdt is Milton {
+    function getVersion() external pure virtual override returns (uint256) {
+        return 4;
+    }
+
     function _getDecimals() internal pure virtual override returns (uint256) {
         return 6;
     }
