922944738fcc597d86f5ff795eed22cf71063915
Mario <mario@ipor.io>
2022-11-18 11:40:40 +0100

    IL-1881 cr fix

--- migrations/libs/contracts/deploy/ipor_token/0001_initial_deploy.js
+++ migrations/libs/contracts/deploy/ipor_token/0001_initial_deploy.js
@@ -3,16 +3,16 @@ const keys = require("../../../json_keys.js");
 const func = require("../../../json_func.js");
 
 module.exports = async function (deployer, _network, addresses, IporToken) {
-    if (!process.env.SC_MIGRATION_INITIAL_IPOR_TOKEN_OWNER) {
+    if (!process.env.SC_MIGRATION_INITIAL_IPOR_TOKEN_DAO_ADDRESS) {
         throw new Error(
-            "Migration stopped! Environment parameter SC_MIGRATION_INITIAL_IPOR_TOKEN_OWNER is not set!"
+            "Migration stopped! Environment parameter SC_MIGRATION_INITIAL_IPOR_TOKEN_DAO_ADDRESS is not set!"
         );
     }
     await deployer.deploy(
         IporToken,
         "IPOR Token",
         "IPOR",
-        process.env.SC_MIGRATION_INITIAL_IPOR_TOKEN_OWNER
+        process.env.SC_MIGRATION_INITIAL_IPOR_TOKEN_DAO_ADDRESS
     );
 
     const iporToken = await IporToken.deployed();
