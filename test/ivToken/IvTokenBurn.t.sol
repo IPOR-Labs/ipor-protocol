7654a2a4b715dda467617055a21cef353cc9fea9
Alvaro Serrano <alvaro@ipor.io>
2022-12-19 18:10:22 +0100

    test: IvTokenBurn

--- test/ivToken/IvTokenBurn.t.sol
+++ test/ivToken/IvTokenBurn.t.sol
@@ -5,13 +5,13 @@ import "forge-std/Test.sol";
 import "../TestCommons.sol";
 import "../../contracts/tokens/IvToken.sol";
 
-contract IvTokenMintTest is Test, TestCommons {
+contract IvTokenBurnTest is Test, TestCommons {
     IvToken internal _ivToken;
     address internal _admin;
     address internal _userOne;
     address internal _userTwo;
 
-    event Mint(address indexed account, uint256 amount);
+    event Burn(address indexed account, uint256 amount);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     function setUp() public {
@@ -21,49 +21,62 @@ contract IvTokenMintTest is Test, TestCommons {
         _userTwo = _getUserAddress(2);
     }
 
-	function testShouldNotMintIvTokenWhenNotStanley () public {
+	function testShouldNotBurnIvTokenWhenNotStanley () public {
 		// given
 		// when
 		// then
 		vm.expectRevert(abi.encodePacked("IPOR_501"));
-		_ivToken.mint(_userOne, 1000000000000000000000000);
+		_ivToken.burn(_userOne, 1000000000000000000000000);
 	}
 
-	function testShouldNotMintIvTokenWhenAmountIsZero() public {
+	function testShouldNotBurnIvTokenWhenAmountIsZero() public {
 		// given
 		address mockIporVaultAddress = _userOne;
 		_ivToken.setStanley(mockIporVaultAddress);
 		// when
 		vm.prank(_userOne);
 		// then
-		vm.expectRevert(abi.encodePacked("IPOR_503"));
-		_ivToken.mint(mockIporVaultAddress, 0);
+		vm.expectRevert(abi.encodePacked("IPOR_504"));
+		_ivToken.burn(mockIporVaultAddress, 0);
 	}
 
-	function testShouldNotMintIvTokenWhenZeroAddress() public {
+	function testShouldNotBurnIvTokenWhenZeroAddress() public {
 		// given
 		address mockIporVaultAddress = _userOne;
 		_ivToken.setStanley(mockIporVaultAddress);
 		// when
 		vm.prank(_userOne);
 		// then
-		vm.expectRevert(abi.encodePacked("ERC20: mint to the zero address"));
-		_ivToken.mint(address(0), 1000000000000000000000000);
+		vm.expectRevert(abi.encodePacked("ERC20: burn from the zero address"));
+		_ivToken.burn(address(0), 1000000000000000000000000);
 	}
 
-	function testShouldMintNewTokens() public {
+	function testShouldNotBurnWhenAmountExceedsBalance() public {
+		// given
+		address mockIporVaultAddress = _userOne;
+		_ivToken.setStanley(mockIporVaultAddress);
+		// when
+		vm.prank(_userOne);
+		// then
+		vm.expectRevert(abi.encodePacked("ERC20: burn amount exceeds balance"));
+		_ivToken.burn(mockIporVaultAddress, 1000000000000000000000001);
+	}
+
+	function testShouldBurnTokens() public {
 		// given
 		address mockIporVaultAddress = _userOne;
 		_ivToken.setStanley(mockIporVaultAddress);
 		uint256 amount = 1000000000000000000000000;
-		// when
 		vm.prank(_userOne);
+		_ivToken.mint(_userOne, 1000000000000000000000000);
+		//when
 		// then
+		vm.prank(_userOne);
 		vm.expectEmit(true, true, false, true);
-		emit Transfer(address(0), _userOne, amount);
+		emit Transfer(_userOne, address(0), amount);
 		vm.expectEmit(true, false, false, true);
-		emit Mint(_userOne, amount);
-		_ivToken.mint(_userOne, amount);
+		emit Burn(_userOne, amount);
+		_ivToken.burn(_userOne, amount);
 	}
 
 	function testShouldEmitEvent() public {
@@ -73,9 +86,10 @@ contract IvTokenMintTest is Test, TestCommons {
 		uint256 amount = 1000000000000000000000000;
 		// when
 		// then
-		vm.expectEmit(true, false, false, true);
-		emit Mint(_userOne, amount);
 		_ivToken.mint(_userOne, amount);
+		vm.expectEmit(true, false, false, true);
+		emit Burn(_userOne, amount);
+		_ivToken.burn(_userOne, amount);
 	}
 }
 
