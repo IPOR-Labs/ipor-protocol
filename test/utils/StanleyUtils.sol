ffee11d6e1d3a8bc90e52959d5ec06bc04c1a288
Alvaro Serrano <alvaro@ipor.io>
2022-12-30 10:20:30 +0100

    feat: mockCase0Stanleys

--- test/utils/StanleyUtils.sol
+++ test/utils/StanleyUtils.sol
@@ -14,6 +14,35 @@ contract StanleyUtils {
 		return mockStanley;
 	}
 
+	function _getMockCase0Stanleys(
+		address tokenUsdt, 
+		address tokenUsdc, 
+		address tokenDai
+	) internal returns (MockCase0Stanley, MockCase0Stanley, MockCase0Stanley){
+		MockCase0Stanley mockStanleyUsdt = new MockCase0Stanley(tokenUsdt);
+		MockCase0Stanley mockStanleyUsdc = new MockCase0Stanley(tokenUsdc);
+		MockCase0Stanley mockStanleyDai = new MockCase0Stanley(tokenDai);
+		return (mockStanleyUsdt, mockStanleyUsdc, mockStanleyDai);
+	}
+
+	function getMockCase0StanleyAddresses(
+		address tokenUsdt, 
+		address tokenUsdc, 
+		address tokenDai
+	) public returns (address[] memory){
+		(
+			MockCase0Stanley stanleyUsdt,
+			MockCase0Stanley stanleyUsdc,
+			MockCase0Stanley stanleyDai
+		) = _getMockCase0Stanleys(address(tokenUsdt), address(tokenUsdc), address(tokenDai));
+		address[] memory mockStanleyAddresses = new address[](3);
+		mockStanleyAddresses[0] = address(stanleyUsdt);
+		mockStanleyAddresses[1] = address(stanleyUsdc);
+		mockStanleyAddresses[2] = address(stanleyDai);
+		return mockStanleyAddresses;
+	}
+	// ------------------------------------------------------------
+
 	function getMockCase1Stanley(address asset) public returns (MockCase1Stanley){
 		MockCase1Stanley mockStanley = new MockCase1Stanley(asset);
 		return mockStanley;
@@ -46,6 +75,8 @@ contract StanleyUtils {
 		mockStanleyAddresses[2] = address(stanleyDai);
 		return mockStanleyAddresses;
 	}
+
+	// ------------------------------------------------------------
 	
 	function getMockCase2Stanley(address asset) public returns (MockCase2Stanley){
 		MockCase2Stanley mockStanley = new MockCase2Stanley(asset);
