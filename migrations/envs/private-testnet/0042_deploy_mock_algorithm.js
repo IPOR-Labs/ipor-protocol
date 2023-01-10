06e30c7cd8eddfd8cc49e832e84c6feb85a75289
Piotr Rzonsowski <pete@ipor.io>
2023-01-10 07:25:29 +0100

    fix itf migration

--- migrations/envs/private-testnet/0042_deploy_mock_algorithm.js
+++ migrations/envs/private-testnet/0042_deploy_mock_algorithm.js
@@ -1,13 +1,19 @@
 const func = require("../../libs/json_func.js");
 const script = require("../../libs/mocks/0004_deploy_mock_ipor_weighted.js");
 const keys = require("../../libs/json_keys.js");
+const itfScript = require("../../libs/itf/deploy/itf_liquidator/dai/0001_initial_deploy.js");
 const IporOracle = artifacts.require("IporOracle");
 const MockIporWeighted = artifacts.require("MockIporWeighted");
 
 module.exports = async function (deployer, _network, addresses) {
     await script(deployer, _network, addresses, MockIporWeighted);
-    const iporOracleProxyAddress = await func.getValue(keys.IporOracleProxy);
+    let iporOracleProxyAddress;
     const iporAlgorithmProxy = await func.getValue(keys.IporAlgorithmProxy);
+    if (process.env.ITF_ENABLED === "true") {
+        iporOracleProxyAddress = await func.getValue(keys.IporOracleProxy);
+    } else {
+        iporOracleProxyAddress = await func.getValue(keys.ItfIporOracleProxy);
+    }
 
     const iporOracleInstance = await IporOracle.at(iporOracleProxyAddress);
     await iporOracleInstance.setAlgorithmAddress(iporAlgorithmProxy);
