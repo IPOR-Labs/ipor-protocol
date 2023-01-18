e6fd425bf462a12b9f20509fc844a3aeef2e3141
Mario <mario@ipor.io>
2023-01-18 22:30:39 +0100

    Prepare mainnet scripts.

--- migrations/envs/mainnet/0066_prepare_upgrade_milton_dai_v3.js
+++ migrations/envs/mainnet/0066_prepare_upgrade_milton_dai_v3.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "../../../.env" });
 const func = require("../../libs/json_func.js");
-const script = require("../../libs/contracts/upgrade/milton/dai/0001_prepare_upgrade_v2.js");
+const script = require("../../libs/contracts/upgrade/milton/dai/0002_prepare_upgrade_v3.js");
 
 module.exports = async function (deployer, _network, addresses) {
     const Milton = artifacts.require("MiltonDai");
