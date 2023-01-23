7ffc357561f8f6474e968870280f51caa27919bd
Piotr Rzonsowski <pete@ipor.io>
2023-01-23 11:59:17 +0100

    rename

--- test/fork/IporOracleSwitchImplementation.t.sol
+++ test/fork/IporOracleSwitchImplementation.t.sol
@@ -10,7 +10,7 @@ import "../../contracts/security/IporOwnable.sol";
 import "../../contracts/libraries/errors/IporErrors.sol";
 import "./IporOracleSnapshot.sol";
 
-contract DaiSwitchIporProtocolImplementation is Test, TestCommons {
+contract SwitchIporOracleImplementation is Test, TestCommons {
 
     address private _dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address private _usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
