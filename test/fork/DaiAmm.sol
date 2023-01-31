c9e2ba0b8eb8dbb7bbba9e0765e66e366e24f762
Piotr Rzonsowski <pete@ipor.io>
2023-01-31 07:41:57 +0100

    open/close swap dai fork test

--- test/fork/DaiAmm.sol
+++ test/fork/DaiAmm.sol
@@ -2,38 +2,89 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "../TestCommons.sol";
-import "../../contracts/amm/pool/Joseph.sol";
-import "../../contracts/amm/Milton.sol";
-import "./IAsset.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../contracts/tokens/IpToken.sol";
+import "../../contracts/tokens/IvToken.sol";
+import "../../contracts/oracles/IporOracle.sol";
+import "../../contracts/vault/StanleyDai.sol";
 import "../../contracts/vault/strategies/StrategyCompound.sol";
 import "../../contracts/vault/strategies/StrategyAave.sol";
+import "../../contracts/amm/pool/Joseph.sol";
+import "../../contracts/amm/pool/JosephDai.sol";
+import "../../contracts/amm/Milton.sol";
+import "../../contracts/amm/MiltonDai.sol";
+import "../../contracts/amm/spread/MiltonSpreadModelDai.sol";
+import "../../contracts/amm/spread/MiltonSpreadModel.sol";
+
 
 
 contract DaiAmm is Test, TestCommons {
 
-    address  private constant _comptrollerAddress = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
-    address  private constant _compTokenAddress = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+    address private constant _algorithmFacade = 0x9D4BD8CB9DA419A9cA1343A5340eD4Ce07E85140;
+    address private constant _comptrollerAddress = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
+    address private constant _compTokenAddress = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
 
     address private constant _addressProviderAave = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5;
     address private constant _stkAave = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
     address private constant _aaveIncentiveAddress = 0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5;
     address private constant _aaveAddress = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
 
-    address public dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public cDai = 0x5d3a536e4d6dbd6114cc1ead35777bab948e3643;
-    address public aDai = 0x028171bCA77440897B824Ca71D1c56caC55b68A3;
+    address public constant dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant cDai = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
+    address public constant aDai = 0x028171bCA77440897B824Ca71D1c56caC55b68A3;
 
+    address public ivDai;
+    address public ipDai;
+
+    IporOracle public iporOracle;
+
+    Stanley public stanley;
     StrategyCompound public strategyCompound;
     StrategyAave public strategyAave;
 
+    Joseph public joseph;
+    Milton public milton;
+    MiltonStorage public miltonStorage;
+    MiltonSpreadModel public miltonSpreadModel;
+
+
     constructor(address owner) {
         vm.startPrank(owner);
+        _createIpDai();
+        _createIvDai();
         _createCompoundStrategy();
         _createAaveStrategy();
+        _createStanley();
+        _createMiltonStorage();
+        _createMiltonSpreadModel();
+        _createIporOracle();
+        _createMilton();
+        _createJoseph();
+        _setupIpToken();
+        _setupIvToken();
+        _setupMilton();
+        _setupMiltonStorage();
+        _setupStanley();
+        _setupStrategyAave();
+        _setupStrategyCompound();
+        _setupIporOracle(owner);
         vm.stopPrank();
+    }
+
+    function approveMiltonJoseph(address user) public {
+        vm.startPrank(user);
+        ERC20(dai).approve(address(joseph), type(uint256).max);
+        ERC20(dai).approve(address(milton), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function _createIpDai() internal {
+        ipDai = address(new IpToken("IP DAI", "IPDAI", dai));
+    }
+
+    function _createIvDai() internal {
+        ivDai = address(new IvToken("IV DAI", "IVDAI", dai));
     }
 
 
@@ -61,5 +112,129 @@ contract DaiAmm is Test, TestCommons {
                 _aaveAddress
             ));
         strategyAave = StrategyAave(address(proxy));
+    }
+
+    function _createStanley() internal {
+        StanleyDai implementation = new StanleyDai();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), abi.encodeWithSignature(
+                "initialize(address,address,address,address)",
+                dai,
+                ivDai,
+                address(strategyAave),
+                address(strategyCompound)
+            ));
+
+        stanley = Stanley(address(proxy));
+    }
+
+    function _createMiltonStorage() internal {
+        MiltonStorageDai implementation = new MiltonStorageDai();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), abi.encodeWithSignature(
+                "initialize()"
+            ));
+        miltonStorage = MiltonStorage(address(proxy));
+    }
+
+    function _createMiltonSpreadModel() internal {
+        miltonSpreadModel = new MiltonSpreadModelDai();
+    }
+
+    function _createIporOracle() internal {
+        IporOracle iporOracleImplementation = new IporOracle();
+        address[] memory assets = new address[](1);
+        assets[0] = address(dai);
+
+        uint32[] memory updateTimestamps = new uint32[](1);
+        updateTimestamps[0] = uint32(1640000000);
+
+        uint64[] memory exponentialMovingAverages = new uint64[](1);
+        exponentialMovingAverages[0] = uint64(32706669664256327);
+
+        uint64[] memory exponentialWeightedMovingVariances = new uint64[](1);
+
+        exponentialWeightedMovingVariances[0] = uint64(49811986068491);
+
+
+        iporOracle = IporOracle(
+            address(
+                new ERC1967Proxy(
+                    address(iporOracleImplementation),
+                    abi.encodeWithSignature(
+                        "initialize(address[],uint32[],uint64[],uint64[])",
+                        assets,
+                        updateTimestamps,
+                        exponentialMovingAverages,
+                        exponentialWeightedMovingVariances
+                    )
+                )
+            )
+        );
+    }
+
+    function _createMilton() internal {
+        MiltonDai implementation = new MiltonDai();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), abi.encodeWithSignature(
+                "initialize(bool,address,address,address,address,address)",
+                false,
+                dai,
+                address(iporOracle),
+                address(miltonStorage),
+                address(miltonSpreadModel),
+                address(stanley)
+            ));
+        milton = Milton(address(proxy));
+    }
+
+    function _createJoseph() internal {
+        JosephDai implementation = new JosephDai();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), abi.encodeWithSignature(
+                "initialize(bool,address,address,address,address,address)",
+                false,
+                dai,
+                ipDai,
+                address(milton),
+                address(miltonStorage),
+                address(stanley)
+            ));
+        joseph = Joseph(address(proxy));
+    }
+
+
+    function _setupMilton() internal {
+        milton.setJoseph(address(joseph));
+        milton.setupMaxAllowanceForAsset(address(joseph));
+        milton.setupMaxAllowanceForAsset(address(stanley));
+
+    }
+
+    function _setupIpToken() internal {
+        IpToken(ipDai).setJoseph(address(joseph));
+    }
+
+    function _setupMiltonStorage() internal {
+        miltonStorage.setJoseph(address(joseph));
+        miltonStorage.setMilton(address(milton));
+    }
+
+    function _setupStanley() internal {
+        stanley.setMilton(address(milton));
+    }
+
+    function _setupIvToken() internal {
+        IvToken(ivDai).setStanley(address(stanley));
+    }
+
+    function _setupStrategyAave() internal {
+        strategyAave.setStanley(address(stanley));
+    }
+
+    function _setupStrategyCompound() internal {
+        strategyCompound.setStanley(address(stanley));
+    }
+
+    function _setupIporOracle(address owner) internal {
+        iporOracle.setIporAlgorithmFacade(_algorithmFacade);
+        iporOracle.addUpdater(owner);
+        iporOracle.updateIndex(dai);
     }
 }
---
+++ test/fork/OpenCloseSwaps.t.sol
@@ -0,0 +1,255 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+
+import "forge-std/Test.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "../TestCommons.sol";
+import "./DaiAmm.sol";
+
+contract AmmDaiFork is Test, TestCommons {
+
+    function testShouldProvideLiquidityFor50000DaiWhenNoAutoRebalanceThreshold() public {
+        // given
+        address user = _getUserAddress(1);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+        daiAmm.joseph().setAutoRebalanceThreshold(0);
+        deal(daiAmm.dai(), user, 500_000e18);
+        daiAmm.approveMiltonJoseph(user);
+
+        uint256 balanceIpDaiBefore = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceDaiBefore = IIpToken(daiAmm.dai()).balanceOf(user);
+
+        // when
+        Joseph joseph = daiAmm.joseph();
+        vm.prank(user);
+        joseph.provideLiquidity(depositAmount);
+
+        //then
+        uint256 balanceIpDaiAfter = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceDaiAfter = IIpToken(daiAmm.dai()).balanceOf(user);
+
+        assertEq(balanceDaiAfter, balanceDaiBefore - depositAmount);
+        assertEq(balanceIpDaiAfter, balanceIpDaiBefore + depositAmount);
+    }
+
+    function testShouldProvideLiquidityFor50000DaiWhenBelowAutoRebalanceThreshold() public {
+        // given
+        address user = _getUserAddress(1);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+        daiAmm.joseph().setAutoRebalanceThreshold(70);
+        deal(daiAmm.dai(), user, 500_000e18);
+        daiAmm.approveMiltonJoseph(user);
+
+        uint256 balanceIpDaiBefore = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceDaiBefore = IIpToken(daiAmm.dai()).balanceOf(user);
+        uint256 balanceStanleyBefore = daiAmm.stanley().totalBalance(address(daiAmm.milton()));
+
+        // when
+        Joseph joseph = daiAmm.joseph();
+        vm.prank(user);
+        joseph.provideLiquidity(depositAmount);
+
+        //then
+        uint256 balanceIpDaiAfter = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceDaiAfter = IIpToken(daiAmm.dai()).balanceOf(user);
+        uint256 balanceStanleyAfter = daiAmm.stanley().totalBalance(address(daiAmm.milton()));
+
+        assertEq(balanceStanleyBefore, 0);
+        assertEq(balanceStanleyAfter, 0);
+        assertEq(balanceDaiAfter, balanceDaiBefore - depositAmount);
+        assertEq(balanceIpDaiAfter, balanceIpDaiBefore + depositAmount);
+    }
+
+    function testShouldProvideLiquidityFor50000DaiWhenAboveAutoRebalanceThreshold() public {
+        // given
+        address user = _getUserAddress(1);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+        Joseph joseph = daiAmm.joseph();
+        joseph.setAutoRebalanceThreshold(40);
+        deal(daiAmm.dai(), user, 500_000e18);
+        daiAmm.approveMiltonJoseph(user);
+
+        uint256 balanceUserIpDaiBefore = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceUserDaiBefore = IIpToken(daiAmm.dai()).balanceOf(user);
+        uint256 balanceStanleyBefore = daiAmm.stanley().totalBalance(address(daiAmm.milton()));
+        uint256 balanceMiltonDaiBefore = IIpToken(daiAmm.dai()).balanceOf(address(daiAmm.milton()));
+
+        // when
+        vm.prank(user);
+        joseph.provideLiquidity(depositAmount);
+
+        //then
+        uint256 balanceUserIpDaiAfter = IIpToken(daiAmm.ipDai()).balanceOf(user);
+        uint256 balanceUserDaiAfter = IIpToken(daiAmm.dai()).balanceOf(user);
+        uint256 balanceStanleyAfter = daiAmm.stanley().totalBalance(address(daiAmm.milton()));
+        uint256 balanceMiltonDaiAfter = IIpToken(daiAmm.dai()).balanceOf(address(daiAmm.milton()));
+
+        assertEq(balanceStanleyBefore, 0);
+        assertEq(balanceStanleyAfter, 7500e18);
+        assertEq(balanceMiltonDaiBefore, 0);
+        assertEq(balanceMiltonDaiAfter, depositAmount - 7500e18);
+        assertEq(balanceUserDaiAfter, balanceUserDaiBefore - depositAmount);
+        assertEq(balanceUserIpDaiAfter, balanceUserIpDaiBefore + depositAmount);
+    }
+
+    function testShouldOpenSwapPayFixed() public {
+        // given
+        address user = _getUserAddress(1);
+        address usertwo = _getUserAddress(2);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+
+        deal(daiAmm.dai(), user, 500_000e18);
+        deal(daiAmm.dai(), usertwo, 500_000e18);
+
+        daiAmm.approveMiltonJoseph(user);
+        daiAmm.approveMiltonJoseph(usertwo);
+
+        Joseph joseph = daiAmm.joseph();
+        vm.prank(usertwo);
+        joseph.provideLiquidity(depositAmount);
+
+        Milton milton = daiAmm.milton();
+
+        // when
+        vm.prank(user);
+        uint256 swapId = milton.openSwapPayFixed(100e18, 9e16, 10e18);
+
+        // then
+        MiltonStorage miltonStorage = daiAmm.miltonStorage();
+        IporTypes.IporSwapMemory memory swap = miltonStorage.getSwapPayFixed(1);
+
+        assertEq(swap.id, 1);
+        assertEq(swap.buyer, user);
+        assertEq(swap.collateral, 64356435643564356436);
+        assertEq(swap.notional, 643564356435643564360);
+        assertEq(swapId, 1);
+    }
+
+    function testShouldOpenSwapReceiveFixed() public {
+        // given
+        address user = _getUserAddress(1);
+        address usertwo = _getUserAddress(2);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+
+        deal(daiAmm.dai(), user, 500_000e18);
+        deal(daiAmm.dai(), usertwo, 500_000e18);
+
+        daiAmm.approveMiltonJoseph(user);
+        daiAmm.approveMiltonJoseph(usertwo);
+
+        Joseph joseph = daiAmm.joseph();
+        vm.prank(usertwo);
+        joseph.provideLiquidity(depositAmount);
+
+        Milton milton = daiAmm.milton();
+
+        // when
+        vm.prank(user);
+        uint256 swapId = milton.openSwapReceiveFixed(100e18, 1e16, 10e18);
+
+        // then
+        MiltonStorage miltonStorage = daiAmm.miltonStorage();
+        IporTypes.IporSwapMemory memory swap = miltonStorage.getSwapReceiveFixed(1);
+
+        console2.log("swap.collateral", swap.collateral);
+        console2.log("swap.notional", swap.notional);
+
+        assertEq(swap.id, 1);
+        assertEq(swap.buyer, user);
+        assertEq(swap.collateral, 64356435643564356436);
+        assertEq(swap.notional, 643564356435643564360);
+        assertEq(swapId, 1);
+    }
+
+    function testShouldCloseSwapPayFixed() public {
+        // given
+        address user = _getUserAddress(1);
+        address userTwo = _getUserAddress(2);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+        MiltonStorage miltonStorage = daiAmm.miltonStorage();
+        Joseph joseph = daiAmm.joseph();
+        Milton milton = daiAmm.milton();
+
+        deal(daiAmm.dai(), user, 500_000e18);
+        deal(daiAmm.dai(), userTwo, 500_000e18);
+
+        daiAmm.approveMiltonJoseph(user);
+        daiAmm.approveMiltonJoseph(userTwo);
+
+        vm.prank(userTwo);
+        joseph.provideLiquidity(depositAmount);
+
+        vm.prank(userTwo);
+        uint256 swapId = milton.openSwapPayFixed(100e18, 9e16, 10e18);
+        IporTypes.IporSwapMemory memory swapBefore = miltonStorage.getSwapPayFixed(1);
+
+        // when
+        vm.prank(userTwo);
+        milton.closeSwapPayFixed(swapId);
+
+        // then
+        IporTypes.IporSwapMemory memory swapAfter = miltonStorage.getSwapPayFixed(1);
+
+        assertEq(swapBefore.id, swapAfter.id);
+        assertEq(swapBefore.buyer, swapAfter.buyer);
+        assertEq(swapBefore.collateral, swapAfter.collateral);
+        assertEq(swapBefore.notional, swapAfter.notional);
+        assertEq(swapBefore.openTimestamp, swapAfter.openTimestamp);
+        assertEq(swapBefore.endTimestamp, swapAfter.endTimestamp);
+        assertEq(swapBefore.idsIndex, swapAfter.idsIndex);
+        assertEq(swapBefore.ibtQuantity, swapAfter.ibtQuantity);
+        assertEq(swapBefore.fixedInterestRate, swapAfter.fixedInterestRate);
+        assertEq(swapBefore.liquidationDepositAmount, swapAfter.liquidationDepositAmount);
+        assertEq(swapBefore.state, 1);
+        assertEq(swapAfter.state, 0);
+    }
+    function testShouldCloseSwapReceiveFixed() public {
+        // given
+        address user = _getUserAddress(1);
+        address userTwo = _getUserAddress(2);
+        uint256 depositAmount = 50_000e18;
+        DaiAmm daiAmm = new DaiAmm(address(this));
+        MiltonStorage miltonStorage = daiAmm.miltonStorage();
+        Joseph joseph = daiAmm.joseph();
+        Milton milton = daiAmm.milton();
+
+        deal(daiAmm.dai(), user, 500_000e18);
+        deal(daiAmm.dai(), userTwo, 500_000e18);
+
+        daiAmm.approveMiltonJoseph(user);
+        daiAmm.approveMiltonJoseph(userTwo);
+
+        vm.prank(userTwo);
+        joseph.provideLiquidity(depositAmount);
+
+        vm.prank(userTwo);
+        uint256 swapId = milton.openSwapReceiveFixed(100e18, 1e16, 10e18);
+        IporTypes.IporSwapMemory memory swapBefore = miltonStorage.getSwapReceiveFixed(1);
+
+        // when
+        vm.prank(userTwo);
+        milton.closeSwapReceiveFixed(swapId);
+
+        // then
+        IporTypes.IporSwapMemory memory swapAfter = miltonStorage.getSwapReceiveFixed(1);
+
+        assertEq(swapBefore.id, swapAfter.id);
+        assertEq(swapBefore.buyer, swapAfter.buyer);
+        assertEq(swapBefore.collateral, swapAfter.collateral);
+        assertEq(swapBefore.notional, swapAfter.notional);
+        assertEq(swapBefore.openTimestamp, swapAfter.openTimestamp);
+        assertEq(swapBefore.endTimestamp, swapAfter.endTimestamp);
+        assertEq(swapBefore.idsIndex, swapAfter.idsIndex);
+        assertEq(swapBefore.ibtQuantity, swapAfter.ibtQuantity);
+        assertEq(swapBefore.fixedInterestRate, swapAfter.fixedInterestRate);
+        assertEq(swapBefore.liquidationDepositAmount, swapAfter.liquidationDepositAmount);
+        assertEq(swapBefore.state, 1);
+        assertEq(swapAfter.state, 0);
+    }
+}
