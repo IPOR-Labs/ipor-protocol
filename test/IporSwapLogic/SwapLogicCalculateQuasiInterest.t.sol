eef217725c02f4b44e084145f79fe78dc9a8eb4b
Alvaro Serrano <alvaro@ipor.io>
2023-02-06 21:14:33 +0100

    test: SwapLogicCalculateQuasiInterest

---
+++ test/IporSwapLogic/SwapLogicCalculateQuasiInterest.t.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.16;
+
+import "forge-std/Test.sol";
+import {DataUtils} from "../utils/DataUtils.sol";
+import "../TestCommons.sol";
+import "../utils/TestConstants.sol";
+import "../../contracts/mocks/MockIporSwapLogic.sol";
+import "../../contracts/mocks/tokens/MockTestnetToken.sol";
+import "../../contracts/interfaces/types/IporTypes.sol";
+
+contract IporSwapLogicCalculateQuasiInterest is Test, TestCommons, DataUtils {
+
+	MockIporSwapLogic internal _iporSwapLogic;
+	MockTestnetToken internal _daiMockedToken;
+
+    function setUp() public {
+    	_iporSwapLogic = new MockIporSwapLogic(); 
+		_daiMockedToken = getTokenDai();
+    }
+
+	function testShouldCalculateQuasiInterestWhenHugeIporAndIBTPriceChanges25DaysLaterAndUserLosesAnd18Decimals () public {
+		// given
+        IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			365 * TestConstants.D16 + 1 * TestConstants.D16, // fixedInterestRate jjj
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_25_DAYS_IN_SECONDS,
+			125 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3893004244800000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestWhenIBTPriceHasNotChanged100DaysLaterAnd18Decimals () public {
+		// given
+        IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate 
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_25_DAYS_IN_SECONDS * 4,
+			120 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3146809564800000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3735237369600000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestCase1() public {
+		// given
+        IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS,
+			1 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3122249099904000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 31126978080000000000000000000000000000000000000);
+	}
+
+	function testShouldRevertWhenClosingTimestampIsLowerThanOpenTimestamp() public {
+		// given
+        IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		vm.expectRevert("IPOR_319");
+		_iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp - TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS,
+			1 * TestConstants.D18
+		);
+	}
+
+	function testShouldCalculateQuasiInterestCase2WhenSameTimestampAndIBTPriceIncreasesAnd18DecimalsCase1 () public {
+		// given
+		IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS,
+			125 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3112697808000000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
+	}
+
+}
