afc92d85d608eb3758082099b17eb68d150fcba7
Alvaro Serrano <alvaro@ipor.io>
2023-02-07 17:00:27 +0100

    test: SwapLogicCalculateQuasiInterest

--- test/IporSwapLogic/SwapLogicCalculateQuasiInterest.t.sol
+++ test/IporSwapLogic/SwapLogicCalculateQuasiInterest.t.sol
@@ -2,21 +2,19 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
+import "forge-std/console2.sol";
 import {DataUtils} from "../utils/DataUtils.sol";
 import "../TestCommons.sol";
 import "../utils/TestConstants.sol";
 import "../../contracts/mocks/MockIporSwapLogic.sol";
-import "../../contracts/mocks/tokens/MockTestnetToken.sol";
 import "../../contracts/interfaces/types/IporTypes.sol";
 
 contract IporSwapLogicCalculateQuasiInterest is Test, TestCommons, DataUtils {
 
 	MockIporSwapLogic internal _iporSwapLogic;
-	MockTestnetToken internal _daiMockedToken;
 
     function setUp() public {
     	_iporSwapLogic = new MockIporSwapLogic(); 
-		_daiMockedToken = getTokenDai();
     }
 
 	function testShouldCalculateQuasiInterestWhenHugeIporAndIBTPriceChanges25DaysLaterAndUserLosesAnd18Decimals () public {
@@ -102,8 +100,8 @@ contract IporSwapLogicCalculateQuasiInterest is Test, TestCommons, DataUtils {
         IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
 			TestConstants.ZERO, // id
 			_admin, // buyer
-			block.timestamp, // openTimestamp
-			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			block.timestamp + TestConstants.PERIOD_50_DAYS_IN_SECONDS, // openTimestamp
+			block.timestamp + TestConstants.PERIOD_75_DAYS_IN_SECONDS, // closeTimestamp
 			TestConstants.ZERO, // idsIndex
 			TestConstants.USD_50_000_18DEC, // collateral
 			9870300000000000000000 * 10, // notional
@@ -139,11 +137,115 @@ contract IporSwapLogicCalculateQuasiInterest is Test, TestCommons, DataUtils {
 		// when
 		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
 			swap, 
-			swap.openTimestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS,
+			swap.openTimestamp,
 			125 * TestConstants.D18
 		);
 		// then
 		assertEq(quasiIFixed, 3112697808000000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestWhenIBTPriceHasNotChanged25DaysLaterAnd18Decimals () public {
+		// given
+		IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_25_DAYS_IN_SECONDS,
+			100 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3121225747200000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3112697808000000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestWhenIBTPriceHasChanged25DaysLaterAnd18Decimals () public {
+		// given
+		IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_25_DAYS_IN_SECONDS,
+			125 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3121225747200000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestWhenIBTPriceHasChanged50DaysLaterAnd18Decimals () public {
+		// given
+		IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_50_DAYS_IN_SECONDS,
+			125 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3129753686400000000000000000000000000000000000000);
+		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
+	}
+
+	function testShouldCalculateQuasiInterestWhenIBTPriceHasChanged50DaysLaterAnd6Decimals () public {
+		// given
+		IporTypes.IporSwapMemory memory swap = IporTypes.IporSwapMemory(
+			TestConstants.ZERO, // id
+			_admin, // buyer
+			block.timestamp, // openTimestamp
+			block.timestamp + TestConstants.SWAP_DEFAULT_PERIOD_IN_SECONDS, // closeTimestamp
+			TestConstants.ZERO, // idsIndex
+			TestConstants.USD_50_000_18DEC, // collateral
+			9870300000000000000000 * 10, // notional
+			987030000000000000000, // ibtQuantity
+			TestConstants.PERCENTAGE_4_18DEC, // fixedInterestRate
+			TestConstants.TC_LIQUIDATION_DEPOSIT_AMOUNT_18DEC, // liquidationDepositAmount
+			1 // state
+		);
+		// when
+		(uint256 quasiIFixed, uint256 quasiIFloating) = _iporSwapLogic.calculateQuasiInterest(
+			swap, 
+			swap.openTimestamp + TestConstants.PERIOD_50_DAYS_IN_SECONDS,
+			125 * TestConstants.D18
+		);
+		// then
+		assertEq(quasiIFixed, 3129753686400000000000000000000000000000000000000);
 		assertEq(quasiIFloating, 3890872260000000000000000000000000000000000000000);
 	}
 
