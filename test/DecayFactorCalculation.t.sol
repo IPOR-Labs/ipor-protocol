82ffba87a90088dbcacc4383e962653ad414650f
Alvaro Serrano <alvaro@ipor.io>
2023-03-15 18:02:09 +0100

    test: DecayFactorCalculation [WIP]

--- test/DecayFactorCalculation.t.sol
+++ test/DecayFactorCalculation.t.sol
@@ -1,34 +1,35 @@
-// SPDX-License-Identifier: UNLICENSED
+//  SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
 import "./TestCommons.sol";
 import "../contracts/mocks/MockDecayFactorCalculation.sol";
+import "forge-std/console2.sol";
+import "forge-std/StdJson.sol";
 
 contract DecayFactorCalculationTest is TestCommons {
-	   
+	using stdJson for string;
+
 	MockDecayFactorCalculation internal _mockDecayFactorCalculation;
     
 	struct LinearFunctionTestData {
-		int256 slope;
 		int256 base;
-		int256 variable;
 		int256 result;
+		int256 slope;
+		int256 variable;
 	}
 
     function setUp() public {
 		_mockDecayFactorCalculation = new MockDecayFactorCalculation();
     }
 
-	function testShouldEvaluateLinearFunction(LinearFunctionTestData memory linearFunctionTestData) public {
-		// given
-		int256 slope = linearFunctionTestData.slope;
-		int256 base = linearFunctionTestData.base;
-		int256 variable = linearFunctionTestData.variable;
-		int256 result = linearFunctionTestData.result;
-		// when 
-		int256 decayFactor = _mockDecayFactorCalculation.linearFunction(slope, base, variable);
-		// then 
-		assertEq(decayFactor, result);
+	function testShouldEvaluateLinearFunction() public {
+		string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/test/asset/testDataForLinearFunction.json");
+        string memory json = vm.readFile(path);
+		bytes memory testData = vm.parseJson(json, "data");
+		LinearFunctionTestData[] memory data = new LinearFunctionTestData[](testData.length);
+		for (uint256 i = 0; i < testData.length; i++) {
+			bytes memory testDataItem = abi.encodePacked(testData[i]);
+		}
 	}
-
 }
