44b7663ca33e568caa0e6250fea05bb0e949bc78
Mario <mario@ipor.io>
2023-04-02 01:15:09 +0200

    Potential vulnerability - PoC in progress...

--- test/fork/IAsset.sol
+++ test/fork/IAsset.sol
@@ -4,4 +4,8 @@ interface IAsset {
     function approve(address _spender, uint256 _value) external;
 
     function balanceOf(address account) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function transfer(address to, uint256 amount) external returns (bool);
 }
