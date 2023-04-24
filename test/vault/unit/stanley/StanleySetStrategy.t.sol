e11351398c0790c47e7b736c945f5369c5ebf3e0
Alvaro Serrano <alvaro@ipor.io>
2023-04-24 19:27:31 +0200

    test: StanleySetStrategy

--- test/vault/unit/stanley/StanleySetStrategy.t.sol
+++ test/vault/unit/stanley/StanleySetStrategy.t.sol
@@ -12,8 +12,8 @@ import {MockTestnetShareTokenCompoundDai} from "../../../../contracts/mocks/toke
 import {IvToken} from "../../../../contracts/tokens/IvToken.sol";
 
 contract StanleySetStrategyTest is TestCommons, DataUtils {
-    MockStrategy internal _strategyAave;
-    MockStrategy internal _strategyCompound;
+    MockStrategy internal _strategyAaveDai;
+    MockStrategy internal _strategyCompoundDai;
     MockTestnetToken internal _daiMockedToken;
     MockTestnetToken internal _usdtMockedToken;
     MockTestnetShareTokenAaveDai internal _aDai;
@@ -29,14 +29,14 @@ contract StanleySetStrategyTest is TestCommons, DataUtils {
         _aDai = getMockTestnetShareTokenAaveDai(TestConstants.TOTAL_SUPPLY_18_DECIMALS);
         _cDai = getMockTestnetShareTokenCompoundDai(TestConstants.TOTAL_SUPPLY_18_DECIMALS);
         _ivTokenDai = new IvToken("IvToken", "IVT", address(_daiMockedToken));
-        _strategyAave = new MockStrategy();
-        _strategyAave.setAsset(address(_daiMockedToken));
-        _strategyAave.setShareToken(address(_aDai));
-        _strategyCompound = new MockStrategy();
-        _strategyCompound.setAsset(address(_daiMockedToken));
-        _strategyCompound.setShareToken(address(_cDai));
+        _strategyAaveDai = new MockStrategy();
+        _strategyAaveDai.setAsset(address(_daiMockedToken));
+        _strategyAaveDai.setShareToken(address(_aDai));
+        _strategyCompoundDai = new MockStrategy();
+        _strategyCompoundDai.setAsset(address(_daiMockedToken));
+        _strategyCompoundDai.setShareToken(address(_cDai));
         _stanleyDai = getStanleyDai(
-            address(_daiMockedToken), address(_ivTokenDai), address(_strategyAave), address(_strategyCompound)
+            address(_daiMockedToken), address(_ivTokenDai), address(_strategyAaveDai), address(_strategyCompoundDai)
         );
         _admin = address(this);
         _userOne = _getUserAddress(1);
@@ -50,37 +50,93 @@ contract StanleySetStrategyTest is TestCommons, DataUtils {
 
     function testShouldSetupAaveStrategy() public {
         // given
-        MockStrategy newStrategyAave = new MockStrategy();
-        newStrategyAave.setShareToken(address(_aDai));
-        newStrategyAave.setAsset(address(_daiMockedToken));
-        address oldStrategyAddress = address(_strategyAave);
-        uint256 newStrategyBalanceBefore = newStrategyAave.balanceOf();
-        _aDai.mint(address(_strategyAave), TestConstants.USD_1_000_18DEC);
-        _strategyAave.setBalance(TestConstants.USD_1_000_18DEC);
+        MockStrategy newStrategyAaveDai = new MockStrategy();
+        newStrategyAaveDai.setShareToken(address(_aDai));
+        newStrategyAaveDai.setAsset(address(_daiMockedToken));
+        address oldStrategyAddress = address(_strategyAaveDai);
+        uint256 newStrategyBalanceBefore = newStrategyAaveDai.balanceOf();
+        _aDai.mint(address(_strategyAaveDai), TestConstants.USD_1_000_18DEC);
+        _strategyAaveDai.setBalance(TestConstants.USD_1_000_18DEC);
         // when
         vm.expectEmit(true, true, true, true);
-        emit StrategyChanged(_admin, address(_strategyAave), address(newStrategyAave), address(_aDai));
-        _stanleyDai.setStrategyAave(address(newStrategyAave));
+        emit StrategyChanged(_admin, address(_strategyAaveDai), address(newStrategyAaveDai), address(_aDai));
+        _stanleyDai.setStrategyAave(address(newStrategyAaveDai));
         // then
-        uint256 newStrategyBalanceAfter = newStrategyAave.balanceOf();
+        uint256 newStrategyBalanceAfter = newStrategyAaveDai.balanceOf();
         assertEq(newStrategyBalanceBefore, newStrategyBalanceAfter);
+    }
+
+    function testShouldSetupAaveStrategyWhenBalanceOnStrategyIsZero() public {
+        // given
+        MockStrategy newStrategyAaveDai = new MockStrategy();
+        newStrategyAaveDai.setShareToken(address(_aDai));
+        newStrategyAaveDai.setAsset(address(_daiMockedToken));
+        address oldStrategyAddress = address(_strategyAaveDai);
+        uint256 oldStrategyBalanceBefore = _strategyAaveDai.balanceOf();
+        uint256 newStrategyBalanceBefore = newStrategyAaveDai.balanceOf();
+        // when
+        vm.expectEmit(true, true, true, true);
+        emit StrategyChanged(_admin, address(_strategyAaveDai), address(newStrategyAaveDai), address(_aDai));
+        _stanleyDai.setStrategyAave(address(newStrategyAaveDai));
+        // then
+        uint256 oldStrategyBalanceAfter = _strategyAaveDai.balanceOf();
+        uint256 newStrategyBalanceAfter = newStrategyAaveDai.balanceOf();
+        assertEq(oldStrategyBalanceBefore, TestConstants.ZERO);
+        assertEq(oldStrategyBalanceAfter, TestConstants.ZERO);
+        assertEq(newStrategyBalanceBefore, TestConstants.ZERO);
+        assertEq(newStrategyBalanceAfter, TestConstants.ZERO);
+        assertEq(address(_stanleyDai.getStrategyAave()), address(newStrategyAaveDai));
+    }
+
+    function testShouldNotSetupNewStrategyAaveWhenUnderlyingTokenDoesNotMatch() public {
+        // given
+        MockStrategy newStrategyAaveDai = new MockStrategy();
+        newStrategyAaveDai.setShareToken(address(_aDai));
+        newStrategyAaveDai.setAsset(address(_usdtMockedToken));
+        // when
+        vm.expectRevert("IPOR_500");
+        _stanleyDai.setStrategyAave(address(newStrategyAaveDai));
+    }
+
+    function testShouldNotSetupNewStrategyAaveWhenZeroAddress() public {
+        // given
+        // when
+        vm.expectRevert("IPOR_000");
+        _stanleyDai.setStrategyAave(address(0));
     }
 
     function testShouldSetupCompoundStrategy() public {
         // given
-        MockStrategy newStrategyCompound = new MockStrategy();
-        newStrategyCompound.setShareToken(address(_cDai));
-        newStrategyCompound.setAsset(address(_daiMockedToken));
-        address oldStrategyAddress = address(_strategyCompound);
-        uint256 newStrategyBalanceBefore = newStrategyCompound.balanceOf();
-        _cDai.mint(address(_strategyCompound), TestConstants.USD_1_000_18DEC);
-        _strategyCompound.setBalance(TestConstants.USD_1_000_18DEC);
+        MockStrategy newStrategyCompoundDai = new MockStrategy();
+        newStrategyCompoundDai.setShareToken(address(_cDai));
+        newStrategyCompoundDai.setAsset(address(_daiMockedToken));
+        address oldStrategyAddress = address(_strategyCompoundDai);
+        uint256 newStrategyBalanceBefore = newStrategyCompoundDai.balanceOf();
+        _cDai.mint(address(_strategyCompoundDai), TestConstants.USD_1_000_18DEC);
+        _strategyCompoundDai.setBalance(TestConstants.USD_1_000_18DEC);
         // when
         vm.expectEmit(true, true, true, true);
-        emit StrategyChanged(_admin, address(_strategyCompound), address(newStrategyCompound), address(_cDai));
-        _stanleyDai.setStrategyCompound(address(newStrategyCompound));
+        emit StrategyChanged(_admin, address(_strategyCompoundDai), address(newStrategyCompoundDai), address(_cDai));
+        _stanleyDai.setStrategyCompound(address(newStrategyCompoundDai));
         // then
-        uint256 newStrategyBalanceAfter = newStrategyCompound.balanceOf();
+        uint256 newStrategyBalanceAfter = newStrategyCompoundDai.balanceOf();
         assertEq(newStrategyBalanceBefore, newStrategyBalanceAfter);
+    }
+
+    function testShouldNotSetupNewStrategyCompoundWhenUnderlyingTokenDoesNotMatch() public {
+        // given
+        MockStrategy newStrategyCompoundDai = new MockStrategy();
+        newStrategyCompoundDai.setShareToken(address(_cDai));
+        newStrategyCompoundDai.setAsset(address(_usdtMockedToken));
+        // when
+        vm.expectRevert("IPOR_500");
+        _stanleyDai.setStrategyCompound(address(newStrategyCompoundDai));
+    }
+
+    function testShouldNotSetupNewStrategyCompoundWhenZeroAddress() public {
+        // given
+        // when
+        vm.expectRevert("IPOR_000");
+        _stanleyDai.setStrategyCompound(address(0));
     }
 }
