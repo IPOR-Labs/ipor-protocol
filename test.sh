9088e1173a2212ab7804b92fc2c8a59074adaa8e
adam <amydlarz@gmail.com>
2023-04-27 12:57:20 +0200

    instant pause manager implementation

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -o pipefail
-
-forge test --fork-url https://eth-mainnet.g.alchemy.com/v2/Ba_wU9f6SEY3rKbZgrDTKihDcPpxiPw0 --match-path "test/fork/*" -vv
-
-forge test --no-match-path "test/fork/*"
-
-npm run test:mainnet
