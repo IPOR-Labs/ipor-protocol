676c11f3b7556166af716840c2ffe1b976d7d85b
kris-ipor <kris@ipor.io>
2023-05-17 09:01:31 +0200

    IL-2860 add get swaps methods

--- contracts/amm/AmmStorageLens.sol
+++ contracts/amm/AmmStorageLens.sol
@@ -4,15 +4,16 @@ pragma solidity 0.8.16;
 import "../interfaces/IAmmStorageLens.sol";
 import "../interfaces/IIporOracle.sol";
 import "../interfaces/IMiltonSpreadModel.sol";
+import "../interfaces/IMiltonStorage.sol";
 
 contract AmmStorageLens is IAmmStorageLens {
     address public immutable usdcAsset;
     address public immutable usdtAsset;
     address public immutable daiAsset;
 
-    address public immutable usdcStorage;
-    address public immutable usdtStorage;
-    address public immutable daiStorage;
+    IMiltonStorage public immutable usdcStorage;
+    IMiltonStorage public immutable usdtStorage;
+    IMiltonStorage public immutable daiStorage;
 
     IIporOracle public immutable iporOracle;
 
@@ -20,9 +21,9 @@ contract AmmStorageLens is IAmmStorageLens {
         address _usdcAsset,
         address _usdtAsset,
         address _daiAsset,
-        address _usdcStorage,
-        address _usdtStorage,
-        address _daiStorage,
+        IMiltonStorage _usdcStorage,
+        IMiltonStorage _usdtStorage,
+        IMiltonStorage _daiStorage,
         IIporOracle _iporOracle
     ) {
         usdcAsset = _usdcAsset;
@@ -39,18 +40,14 @@ contract AmmStorageLens is IAmmStorageLens {
         view
         override
         returns (int256 spreadPayFixed, int256 spreadReceiveFixed)
-    {
-    }
+    {}
 
     function _calculateSpread(address asset, uint256 calculateTimestamp)
-    internal
-    view
-    returns (int256 spreadPayFixed, int256 spreadReceiveFixed)
+        internal
+        view
+        returns (int256 spreadPayFixed, int256 spreadReceiveFixed)
     {
-        IporTypes.AccruedIpor memory accruedIpor = _iporOracle.getAccruedIndex(
-            calculateTimestamp,
-            _asset
-        );
+        IporTypes.AccruedIpor memory accruedIpor = _iporOracle.getAccruedIndex(calculateTimestamp, _asset);
 
         IporTypes.MiltonBalancesMemory memory balance = _getAccruedBalance(asset);
 
@@ -60,7 +57,50 @@ contract AmmStorageLens is IAmmStorageLens {
         spreadReceiveFixed = miltonSpreadModel.calculateSpreadReceiveFixed(accruedIpor, balance);
     }
 
-    function getStorageImplementation(address asset) internal pure returns (address) {
+    function getSwapsPayFixed(
+        address asset,
+        address account,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view override returns (uint256 totalCount, IAmmStorageLens.IporSwap[] memory swaps) {
+        return getStorageImplementation(asset).getSwapsPayFixed(account, offset, chunkSize);
+        //TODO calculate values
+    }
+
+    function getSwapsReceiveFixed(
+        address asset,
+        address account,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view override returns (uint256 totalCount, IAmmStorageLens.IporSwap[] memory swaps) {
+        return getStorageImplementation(asset).getSwapsReceiveFixed(account, offset, chunkSize);
+    }
+
+    function getMySwapsPayFixed(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view override returns (uint256 totalCount, IAmmStorageLens.IporSwap[] memory swaps) {
+        return getStorageImplementation(asset).getSwapsPayFixed(_msgSender(), offset, chunkSize);
+    }
+
+    function getMySwapsReceiveFixed(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view override returns (uint256 totalCount, IAmmStorageLens.IporSwap[] memory swaps) {
+        return getStorageImplementation(asset).getSwapsReceiveFixed(_msgSender(), offset, chunkSize);
+    }
+
+    function getMySwaps(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IAmmStorageLens.IporSwap[] memory swaps) {
+        return getStorageImplementation(asset).getSwaps(_msgSender(), offset, chunkSize);
+    }
+
+    function getStorageImplementation(address asset) internal pure returns (IMiltonStorage) {
         if (asset == usdcAsset) {
             return usdcStorage;
         } else if (asset == usdtAsset) {
--- contracts/interfaces/IAmmStorageLens.sol
+++ contracts/interfaces/IAmmStorageLens.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.16;
 
+import "./types/IporTypes.sol";
+
 interface IAmmStorageLens {
     /// @notice Calculates spread for the current block.
     /// @dev All values represented in 18 decimals.
@@ -53,4 +55,103 @@ interface IAmmStorageLens {
         external
         view
         returns (uint256 closableStatus);
+
+    /// @notice Gets the list of active Pay Fixed Receive Floating swaps in Milton for a given asset and address
+    /// @param asset asset / stablecoin address
+    /// @param account account address for which list of swaps is scoped
+    /// @param offset offset for paging
+    /// @param chunkSize page size for paging
+    /// @return totalCount total number of active Pay Fixed swaps in Milton
+    /// @return swaps list of active swaps for a given filter
+    function getSwapsPayFixed(
+        address asset,
+        address account,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IporSwap[] memory swaps);
+
+    /// @notice Gets the list of active Receive Fixed Pay Floating Swaps in Milton for a given asset and address
+    /// @param asset asset / stablecoin address
+    /// @param account account address for which list of swaps is scoped
+    /// @param offset offset for paging
+    /// @param chunkSize page size for paging
+    /// @return totalCount total number of Receive Fixed swaps in Milton
+    /// @return swaps list of active swaps for a given filter
+    function getSwapsReceiveFixed(
+        address asset,
+        address account,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IporSwap[] memory swaps);
+
+    /// @notice Gets list of active Pay Fixed Receive Floating Swaps in Milton of sender for a given asset
+    /// @param asset asset / stablecoin address
+    /// @param offset offset for paging
+    /// @param chunkSize page size for paging
+    /// @return totalCount total number of Pay Fixed swaps in Milton for a current user
+    /// @return swaps list of active swaps for a given asset
+    function getMySwapsPayFixed(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IporSwap[] memory swaps);
+
+    /// @notice Gets list of active Receive Fixed Pay Floating Swaps in Milton of sender for a given asset
+    /// @param asset asset / stablecoin address
+    /// @param offset offset for paging functionality purposes
+    /// @param chunkSize page size for paging functionality purposes
+    /// @return totalCount total amount of Receive Fixed swaps in Milton for a current user
+    /// @return swaps list of active swaps for a given asset
+    function getMySwapsReceiveFixed(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IporSwap[] memory swaps);
+
+    /// @notice Gets active swaps for a given asset sender address (aka buyer).
+    /// @param asset asset address
+    /// @param offset offset for paging
+    /// @param chunkSize page size for paging
+    /// @return totalCount total number of sender's active swaps in Milton
+    /// @return swaps list of active sender's swaps
+    function getMySwaps(
+        address asset,
+        uint256 offset,
+        uint256 chunkSize
+    ) external view returns (uint256 totalCount, IporSwap[] memory swaps);
+
+    /// @notice IPOR Swap structure.
+    struct IporSwap {
+        /// @notice Swap ID.
+        uint256 id;
+        /// @notice Swap asset (stablecoint / underlying token)
+        address asset;
+        /// @notice Swap's buyer
+        address buyer;
+        /// @notice Swap collateral, represented in 18 decimals.
+        uint256 collateral;
+        /// @notice Notional amount, represented in 18 decimals.
+        uint256 notional;
+        /// @notice Swap leverage, represented in 18 decimals.
+        uint256 leverage;
+        /// @notice Swap direction
+        /// @dev 0 - Pay Fixed-Receive Floating, 1 - Receive Fixed - Pay Floading
+        uint8 direction;
+        /// @notice Swap's notional amount denominated in the Interest Bearing Token (IBT)
+        /// @dev value represented in 18 decimals
+        uint256 ibtQuantity;
+        /// @notice Fixed interest rate.
+        uint256 fixedInterestRate;
+        /// @notice Current position value, represented in 18 decimals.
+        int256 payoff;
+        /// @notice Moment when swap was opened.
+        uint256 openTimestamp;
+        /// @notice Mopment when swap achieve its maturity.
+        uint256 endTimestamp;
+        /// @notice Liqudidation deposit value on day when swap was opened. Value represented in 18 decimals.
+        uint256 liquidationDepositAmount;
+        /// @notice State of the swap
+        /// @dev 0 - INACTIVE, 1 - ACTIVE
+        uint256 state;
+    }
 }
