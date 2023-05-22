632138c5c70d521209bb410eb3abd2aff42e7bd0
kris-ipor <kris@ipor.io>
2023-05-22 08:10:27 +0200

    IL-2890 clean up interest rates calculation lib

--- contracts/libraries/math/InterestRates.sol
+++ contracts/libraries/math/InterestRates.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.16;
 
-import "forge-std/console2.sol";
 import "abdk-libraries-solidity/ABDKMathQuad.sol";
 import "../Constants.sol";
 import "./IporMath.sol";
