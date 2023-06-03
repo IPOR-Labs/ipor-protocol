cd3e1dfd35bdb66e13142e4ff1829b5d9abc9c1d
Mario <mario@ipor.io>
2023-06-03 21:57:21 +0200

    Remove migration.

--- migrations/envs/mainnet/0079_prepare_upgrade_milton_storage_dai_v2.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({ path: "../../../.env" });
-const func = require("../../libs/json_func.js");
-const script = require("../../libs/contracts/upgrade/milton_storage/dai/0001_prepare_upgrade_v2.js");
-
-module.exports = async function (deployer, _network, addresses) {
-    const MiltonStorage = artifacts.require("MiltonStorageDai");
-    await script(deployer, _network, addresses, MiltonStorage);
-    await func.updateLastCompletedMigration();
-};
--- migrations/libs/itf/deploy/spread_model/usdc/0001_initial_deploy.js
+++
@@ -1,9 +0,0 @@
-const keys = require("../../../../json_keys.js");
-const func = require("../../../../json_func.js");
-
-module.exports = async function (deployer, _network, addresses, ItfMiltonSpreadModelUsdc) {
-    await deployer.deploy(ItfMiltonSpreadModelUsdc);
-    const itfMiltonSpreadModelUsdc = await ItfMiltonSpreadModelUsdc.deployed();
-
-    await func.update(keys.ItfMiltonSpreadModelUsdc, itfMiltonSpreadModelUsdc.address);
-};
