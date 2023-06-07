419ddb803792b644146242ae98a3e09f68ba740d
Kris <kris@ipor.io>
2023-06-07 13:57:18 +0200

    IL-2961 redeem test on fork

--- test/fork/FirstForkTest.t.sol
+++ test/fork/FirstForkTest.t.sol
@@ -25,14 +25,22 @@ contract FirstForkTest is TestForkCommons {
         vm.prank(user);
         IAmmPoolsService(iporProtocolRouterProxy).provideLiquidityDai(user, 10_000e18);
 
-        // then
         uint256 balanceIpDaiAfter = ERC20(ipDAI).balanceOf(user);
         uint256 balanceDaiAfter = ERC20(DAI).balanceOf(user);
 
+        vm.prank(user);
+        IAmmPoolsService(iporProtocolRouterProxy).redeemFromAmmPoolDai(user, balanceIpDaiAfter);
+
+        uint256 balanceIpDaiAfterRedeem = ERC20(ipDAI).balanceOf(user);
+        uint256 balanceDaiAfterRedeem = ERC20(DAI).balanceOf(user);
+
+        // then
         console2.log("balanceIpDaiBefore", balanceIpDaiBefore);
         console2.log("balanceDaiBefore", balanceDaiBefore);
         console2.log("balanceIpDaiAfter", balanceIpDaiAfter);
         console2.log("balanceDaiAfter", balanceDaiAfter);
+        console2.log("balanceIpDaiAfterRedeem", balanceIpDaiAfterRedeem);
+        console2.log("balanceDaiAfterRedeem", balanceDaiAfterRedeem);
     }
 
     function testUSDC() public {
@@ -52,14 +60,22 @@ contract FirstForkTest is TestForkCommons {
         vm.prank(user);
         IAmmPoolsService(iporProtocolRouterProxy).provideLiquidityUsdc(user, 10_000e6);
 
-        // then
         uint256 balanceIpUsdcAfter = ERC20(ipUSDC).balanceOf(user);
         uint256 balanceUsdcAfter = ERC20(USDC).balanceOf(user);
 
+        vm.prank(user);
+        IAmmPoolsService(iporProtocolRouterProxy).redeemFromAmmPoolUsdc(user, balanceIpUsdcAfter);
+
+        uint256 balanceIpUsdcAfterRedeem = ERC20(ipUSDC).balanceOf(user);
+        uint256 balanceUsdcAfterRedeem = ERC20(USDC).balanceOf(user);
+
+        // then
         console2.log("balanceIpUsdcBefore", balanceIpUsdcBefore);
         console2.log("balanceUsdcBefore", balanceUsdcBefore);
         console2.log("balanceIpUsdcAfter", balanceIpUsdcAfter);
         console2.log("balanceUsdcAfter", balanceUsdcAfter);
+        console2.log("balanceIpUsdcAfterRedeem", balanceIpUsdcAfterRedeem);
+        console2.log("balanceUsdcAfterRedeem", balanceUsdcAfterRedeem);
     }
 
     function testUSDT() public {
@@ -79,13 +95,21 @@ contract FirstForkTest is TestForkCommons {
         vm.prank(user);
         IAmmPoolsService(iporProtocolRouterProxy).provideLiquidityUsdt(user, 10_000e6);
 
-        // then
         uint256 balanceIpUsdtAfter = ERC20(ipUSDT).balanceOf(user);
-        uint256 balanceUsdtAfter = IAsset(USDT).balanceOf(user);
+        uint256 balanceUsdtAfter = ERC20(USDT).balanceOf(user);
 
+        vm.prank(user);
+        IAmmPoolsService(iporProtocolRouterProxy).redeemFromAmmPoolUsdt(user, balanceIpUsdtAfter);
+
+        uint256 balanceIpUsdtAfterRedeem = ERC20(ipUSDT).balanceOf(user);
+        uint256 balanceUsdtAfterRedeem = ERC20(USDT).balanceOf(user);
+
+        // then
         console2.log("balanceIpUsdtBefore", balanceIpUsdtBefore);
         console2.log("balanceUsdtBefore", balanceUsdtBefore);
         console2.log("balanceIpUsdtAfter", balanceIpUsdtAfter);
         console2.log("balanceUsdtAfter", balanceUsdtAfter);
+        console2.log("balanceIpUsdtAfterRedeem", balanceIpUsdtAfterRedeem);
+        console2.log("balanceUsdtAfterRedeem", balanceUsdtAfterRedeem);
     }
 }
