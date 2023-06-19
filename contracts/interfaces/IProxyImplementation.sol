16dec08c90af96a291ac74d2752f13c0aab91a74
Piotr Rzonsowski <pete@ipor.io>
2023-06-19 08:26:37 +0200

    add doc

--- contracts/interfaces/IProxyImplementation.sol
+++ contracts/interfaces/IProxyImplementation.sol
@@ -2,5 +2,8 @@
 pragma solidity 0.8.20;
 
 interface IProxyImplementation {
+    /// @notice Retrieves the address of the implementation contract for UUPS proxy.
+    /// @return The address of the implementation contract.
+    /// @dev The function returns the value stored in the implementation storage slot.
     function getImplementation() external view returns (address);
 }
