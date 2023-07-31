83abe0f12574d36d70c5dbc7dac2daaf6aa882d1
Mario <mario@ipor.io>
2023-07-31 22:28:25 +0200

    Fix migration.

--- migrations/libs/contracts/upgrade/stanley/dai/0001_prepare_upgrade_v3.js
+++ migrations/libs/contracts/upgrade/stanley/dai/0001_prepare_upgrade_v3.js
@@ -9,13 +9,14 @@ module.exports = async function (deployer, _network, addresses, Stanley) {
     const strategyAave = await func.getValue(keys.AaveStrategyProxyDai);
     const strategyCompound = await func.getValue(keys.CompoundStrategyProxyDai);
     const strategyDsr = await func.getValue(keys.DsrStrategyProxyDai);
+    const ivToken = await func.getValue(keys.ivDAI);
 
     const stanleyProxyAddress = await func.getValue(keys.StanleyProxyDai);
 
     const stanleyImplAddress = await prepareUpgrade(stanleyProxyAddress, Stanley, {
         unsafeAllow: ["constructor", "state-variable-immutable"],
         unsafeAllowRenames: true,
-        constructorArgs: [asset, milton, strategyAave, strategyCompound, strategyDsr],
+        constructorArgs: [asset, milton, strategyAave, strategyCompound, strategyDsr, ivToken],
         deployer: deployer,
         kind: "uups",
     });
