7b3bd9f215c6bc3d2023131f0267c9ee9f08d28e
Mario <mario@ipor.io>
2023-08-03 19:53:26 +0200

    IL-3226 I2: Use type(uint256).max instead of integer literal

--- contracts/libraries/Constants.sol
+++ contracts/libraries/Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.20;
 
 library Constants {
-    uint256 public constant MAX_VALUE = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+    uint256 public constant MAX_VALUE = type(uint256).max;
     uint256 public constant WAD_LEVERAGE_1000 = 1_000e18;
     uint256 public constant YEAR_IN_SECONDS = 365 days;
     uint256 public constant MAX_CHUNK_SIZE = 50;
