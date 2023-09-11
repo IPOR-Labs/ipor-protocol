c1ea260ce32ada589238c8f54790094644029551
Mario <mario@ipor.io>
2023-09-11 14:02:47 +0200

    Remove method.

--- test/fork/IIpTokenV1.sol
+++ test/fork/IIpTokenV1.sol
@@ -2,6 +2,5 @@
 pragma solidity 0.8.20;
 
 interface IIpTokenV1 {
-    function setJoseph(address newRouter) external;
-    function getJoseph() external view returns (address);
+    function setJoseph(address newRouter) external;;
 }
