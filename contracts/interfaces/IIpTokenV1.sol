5e75611bb831e70cceffd52394054f4db06677b3
Mario <mario@ipor.io>
2023-09-11 19:45:13 +0200

    Typo.

--- contracts/interfaces/IIpTokenV1.sol
+++ contracts/interfaces/IIpTokenV1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
-/// @title Interface do IpToken in version 1.
+/// @title Interface to IpToken in version 1.
 interface IIpTokenV1 {
     function setJoseph(address newRouter) external;
 
