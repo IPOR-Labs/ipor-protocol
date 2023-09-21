8d0217426305858eb22ded0b12a60901e468fefa
Mario <mario@ipor.io>
2023-09-21 08:58:20 +0200

    CR Fix.

--- test/vault/MockStrategyWithTransfers.sol
+++ test/vault/MockStrategyWithTransfers.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-//IERC20Upgradeable
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "../../contracts/interfaces/IStrategy.sol";
 import "../../contracts/interfaces/IIporContractCommonGov.sol";
