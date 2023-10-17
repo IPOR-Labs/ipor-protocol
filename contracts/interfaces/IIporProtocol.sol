8abcba65ac31f9c30e7b2d1f0c94e0d8da8daa98
rav <rav@ipor.io>
2023-10-17 19:07:48 +0200

    IL-3627 Add ISpreadRouter interface and update IIporProtocol interface

--- contracts/interfaces/IIporProtocol.sol
+++ contracts/interfaces/IIporProtocol.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts-upgradeable/interfaces/draft-IERC1822Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC1967Upgradeable.sol";
+import "./IIporContractCommonGov.sol";
+import "./IProxyImplementation.sol";
 import "./IAmmSwapsLens.sol";
 import "./IAmmPoolsLens.sol";
 import "./IAssetManagementLens.sol";
@@ -15,8 +19,13 @@ import "./IPowerTokenFlowsService.sol";
 import "../amm-eth/interfaces/IAmmPoolsLensEth.sol";
 import "../amm-eth/interfaces/IAmmPoolsServiceEth.sol";
 
-/// @title Interface for interaction with IPOR protocol. Interface combines all IPOR protocol services and lenses.
+/// @title Interface for interaction with IPOR protocol.
+/// Interface combines all IporProtocolRouter interfaces and supported services and lenses by router.
 interface IIporProtocol is
+    IERC1822ProxiableUpgradeable,
+    IERC1967Upgradeable,
+    IIporContractCommonGov,
+    IProxyImplementation,
     IAmmSwapsLens,
     IAmmPoolsLens,
     IAssetManagementLens,
