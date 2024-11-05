4c2d023aebe74ae5e1599990a540dbdb246b1a5f
Mario <mario@ipor.io>
2024-11-06 00:17:51 +0100

    .

---
+++ contracts/base/amm-usdc/services/AmmCloseSwapServiceUsdcBaseV2.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.26;
+
+import {AmmTypes} from "../../../interfaces/types/AmmTypes.sol";
+import {IAmmCloseSwapLens} from "../../../interfaces/IAmmCloseSwapLens.sol";
+import {IAmmCloseSwapServiceUsdc} from "../../../interfaces/IAmmCloseSwapServiceUsdc.sol";
+import {AmmCloseSwapServiceBaseV2} from "../../../base/amm/services/AmmCloseSwapServiceBaseV2.sol";
+import {StorageLibBaseV1} from "../../libraries/StorageLibBaseV1.sol";
+
+/// @dev It is not recommended to use service contract directly, should be used only through IporProtocolRouter.
+/// @dev Service can be safely used directly only if you are sure that methods will not touch any storage variables.
+contract AmmCloseSwapServiceUsdcBaseV2 is AmmCloseSwapServiceBaseV2, IAmmCloseSwapServiceUsdc {
+    constructor(
+        IAmmCloseSwapLens.AmmCloseSwapServicePoolConfiguration memory poolCfg,
+        address iporOracle_
+    ) AmmCloseSwapServiceBaseV2(poolCfg, iporOracle_) {}
+
+    function closeSwapsUsdc(
+        address beneficiary,
+        uint256[] memory payFixedSwapIds,
+        uint256[] memory receiveFixedSwapIds,
+        AmmTypes.CloseSwapRiskIndicatorsInput calldata riskIndicatorsInput
+    )
+        external
+        override
+        returns (
+            AmmTypes.IporSwapClosingResult[] memory closedPayFixedSwaps,
+            AmmTypes.IporSwapClosingResult[] memory closedReceiveFixedSwaps
+        )
+    {
+        (closedPayFixedSwaps, closedReceiveFixedSwaps) = _closeSwaps(
+            beneficiary,
+            payFixedSwapIds,
+            receiveFixedSwapIds,
+            riskIndicatorsInput
+        );
+    }
+
+    function emergencyCloseSwapsUsdc(
+        uint256[] memory payFixedSwapIds,
+        uint256[] memory receiveFixedSwapIds,
+        AmmTypes.CloseSwapRiskIndicatorsInput calldata riskIndicatorsInput
+    )
+        external
+        override
+        returns (
+            AmmTypes.IporSwapClosingResult[] memory closedPayFixedSwaps,
+            AmmTypes.IporSwapClosingResult[] memory closedReceiveFixedSwaps
+        )
+    {
+        (closedPayFixedSwaps, closedReceiveFixedSwaps) = _emergencyCloseSwaps(
+            payFixedSwapIds,
+            receiveFixedSwapIds,
+            riskIndicatorsInput
+        );
+    }
+
+    function _getMessageSigner() internal view override returns (address) {
+        return StorageLibBaseV1.getMessageSignerStorage().value;
+    }
+}
